01cc0eb9bb3b36ec8c4e572864367ffdba61f037
Link Satonaka <link.satonaka@outlook.com>
2015-09-25 02:34:49 -0400

    ditto pkg

---
+++ ditto/tools/chocolateyInstall.ps1
@@ -0,0 +1,8 @@
+﻿$packageName = 'ditto'
+$url = 'http://skylineservers.dl.sourceforge.net/project/ditto-cp/Ditto/3.20.74.0/DittoSetup_3_20_74_0.exe'
+$url64 = 'http://skylineservers.dl.sourceforge.net/project/ditto-cp/Ditto/3.20.74.0/DittoSetup_64bit_3_20_74_0.exe'
+$installerType = 'exe'
+$silentArgs = '/sp- /silent /norestart'
+# The installer doesn't properly kill ditto
+Get-Process | Where { $_.name -eq 'ditto' } | Stop-Process
+Install-ChocolateyPackage $packageName $installerType $silentArgs $url $url64
