c570117c54d1b9a4ee6990332edf47b86303dfb8
Link Satonaka <link.satonaka@outlook.com>
2015-09-25 02:11:58 -0400

    clarity & cleanup

--- mpv/tools/chocolateyinstall.ps1
+++ mpv/tools/chocolateyinstall.ps1
@@ -1,18 +1,18 @@
-﻿$ErrorActionPreference = 'Stop'; # stop on all errors
+﻿$ErrorActionPreference = 'Stop';
 
-$packageName = 'mpv' # arbitrary name for the package, used in messages
+$packageName = 'mpv'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url = 'http://mpv.srsfckn.biz/mpv-i686-20150923.7z' # download url
-$url64 = 'http://mpv.srsfckn.biz/mpv-x86_64-20150923.7z' # 64bit URL here or remove - if installer is both, use $url
+$url = 'http://mpv.srsfckn.biz/mpv-i686-20150923.7z'
+$url64 = 'http://mpv.srsfckn.biz/mpv-x86_64-20150923.7z'
 
 Install-ChocolateyZipPackage $packageName $url $toolsDir $url64
 Get-ChocolateyWebFile $packageName "$toolsDir\mpv-install.zip" 'https://github.com/rossy/mpv-install/archive/master.zip'
 Get-ChocolateyUnzip "$toolsDir\mpv-install.zip" $toolsDir
 Move-Item -Path "$toolsDir\mpv-install-master\*" -Destination $toolsDir
 Remove-Item -Path "$toolsDir\mpv-install-master"
-# This failed to launch the process as admin, leaving it here until chocolaty is fixed
+# This failed to launch the process as admin, commenting out until chocolaty is fixed
 #Start-ChocolateyProcessAsAdmin "/K $toolsDir\mpv-install.bat" 'cmd.exe' -validExitCodes $validExitCodes
 Start-Process cmd.exe -ArgumentList "/C $toolsDir\mpv-install.bat" -Verb Runas
-# Shim doesn't work with mpv as expected, so I'm doing away with it
+# Shim not necessary for mpv (also mpv.com isn't shimmed), replacing with hardlinks
 Start-Process cmd.exe -ArgumentList "/C MKLINK /H $ENV:ChocolateyInstall\bin\mpv.exe $toolsdir\mpv.exe"
 Start-Process cmd.exe -ArgumentList "/C MKLINK /H $ENV:ChocolateyInstall\bin\mpv.com $toolsdir\mpv.com"
