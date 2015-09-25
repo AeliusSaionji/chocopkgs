d30e010c6c6f8550417e06e39f75049926854f79
Link Satonaka <link.satonaka@outlook.com>
2015-09-25 02:54:48 -0400

    custom cygwin

---
+++ Cygwin/tools/chocolateyInstall.ps1
@@ -0,0 +1,6 @@
+﻿$packageName = 'Cygwin'
+$url = 'http://cygwin.com/setup-x86.exe'
+$url64 = 'http://cygwin.com/setup-x86_64.exe'
+$installerType = 'exe'
+$silentArgs = '--no-admin --quiet-mode -g -d -o -l c:\cygwin64\tmp -P irssi,openssh,curl,rsync,zsh,git,make,perl-Text-CharWidth,perl-Algorithm-Diff -s http://mirrors.kernel.org/sourceware/cygwin/'
+Install-ChocolateyPackage $packageName $installerType $silentArgs $url $url64
