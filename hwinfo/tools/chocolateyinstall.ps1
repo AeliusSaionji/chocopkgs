d7867cc362c5c9f49b626fab4b9d25f0f64ba40a
Link Satonaka <link.satonaka@outlook.com>
2015-09-26 14:35:45 -0400

    hwinfo install

---
+++ hwinfo/tools/chocolateyinstall.ps1
@@ -0,0 +1,10 @@
+﻿$PSScriptRoot = Split-Path -Parent $MyInvocation.MyCommand.Definition
+Import-Module (Join-Path $PSScriptRoot 'get-fosshublinks.ps1')
+
+$packageName = 'hwinfo'
+$url = Get-FosshubLinks "http://www.fosshub.com/genLink/HWiNFO/hw32_504.exe"
+$url64 = Get-FosshubLinks "http://www.fosshub.com/genLink/HWiNFO/hw64_504.exe"
+$fileType = 'exe'
+$silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART'
+
+Install-ChocolateyPackage $packageName $fileType $silentArgs $url $url64
---
+++ hwinfo/tools/chocolateyuninstall.ps1
@@ -0,0 +1,32 @@
+﻿$packageName = 'hwinfo'
+$shouldUninstall = $true
+
+$machine_key   = 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\HWiNFO64_is1'
+# 32bit version will be here on a 32bit OS
+$machine_key32  = 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\HWiNFO32_is1'
+# If you installed the 32bit version on a 64bit OS you're doing it wrong
+# But I'm covering that eventuality here anyway.
+$machine_key6432 = 'HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\HWiNFO32_is1'
+
+$file = @($machine_key, $machine_key32, $machine_key6432) `
+    | ?{ Test-Path $_ } `
+    | Get-ItemProperty `
+    | Select-Object -ExpandProperty UninstallString
+
+if ($file -eq $null -or $file -eq '') {
+    Write-Host "$packageName has already been uninstalled by other means."
+    $shouldUninstall = $false
+}
+
+$installerType = 'EXE' 
+$silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART'
+$validExitCodes = @(0)
+
+#if (!(Test-Path $file)) {
+#    Write-Host "$packageName has already been uninstalled by other means."
+#    $shouldUninstall = $false
+#}
+
+if ($shouldUninstall) {
+ Uninstall-ChocolateyPackage -PackageName $packageName -FileType $installerType -SilentArgs $silentArgs -validExitCodes $validExitCodes -File $file
+}
