0821dc9166ddc27c2b9bb367233d550f27f5b53d
Link Satonaka <link.satonaka@outlook.com>
2015-09-26 15:16:17 -0400

    reorder things, use rawgit cdn

--- mpv/tools/chocolateyinstall.ps1
+++ mpv/tools/chocolateyinstall.ps1
@@ -5,7 +5,6 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url = 'http://mpv.srsfckn.biz/mpv-i686-20150923.7z'
 $url64 = 'http://mpv.srsfckn.biz/mpv-x86_64-20150923.7z'
 
-$installbat = Read-Host 'Do you wish to register mpv as a video player with the system? (yes/no)'
 Install-ChocolateyZipPackage $packageName $url $toolsDir $url64
 # Remove hardlinks before attempting to recreate them
 If (Test-Path -Path "$ENV:ChocolateyInstall\bin\mpv.exe") {
@@ -14,11 +13,13 @@ If (Test-Path -Path "$ENV:ChocolateyInstall\bin\mpv.exe") {
 If (Test-Path -Path "$ENV:ChocolateyInstall\bin\mpv.com") {
 	Remove-Item -Path "$ENV:ChocolateyInstall\bin\mpv.com"
 }
+# Shim not necessary for mpv (also mpv.com isn't shimmed), replacing with hardlinks
 Start-Process cmd.exe -ArgumentList "/C MKLINK /H $ENV:ChocolateyInstall\bin\mpv.exe $toolsdir\mpv.exe"
 Start-Process cmd.exe -ArgumentList "/C MKLINK /H $ENV:ChocolateyInstall\bin\mpv.com $toolsdir\mpv.com"
 
+$installbat = Read-Host 'Do you wish to register mpv as a video player with the system? (yes/no)'
 If ($installbat -eq 'yes') {
-	Get-ChocolateyWebFile $packageName "$toolsDir\mpv-install.zip" 'https://github.com/rossy/mpv-install/archive/master.zip'
+	Get-ChocolateyWebFile $packageName "$toolsDir\mpv-install.zip" 'https://cdn.rawgit.com/rossy/mpv-install/archive/master.zip'
 	Get-ChocolateyUnzip "$toolsDir\mpv-install.zip" $toolsDir
 	Move-Item -Path "$toolsDir\mpv-install-master\*" -Destination $toolsDir -Force
 	Remove-Item -Path "$toolsDir\mpv-install-master"
@@ -26,4 +27,3 @@ If ($installbat -eq 'yes') {
 	#Start-ChocolateyProcessAsAdmin "/K $toolsDir\mpv-install.bat" 'cmd.exe' -validExitCodes $validExitCodes
 	Start-Process cmd.exe -ArgumentList "/C $toolsDir\mpv-install.bat" -Verb Runas
 }
-# Shim not necessary for mpv (also mpv.com isn't shimmed), replacing with hardlinks
