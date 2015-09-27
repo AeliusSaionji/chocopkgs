829c00c471c9dea3c4100c6b9accd7b469cefdd8
Link Satonaka <link.satonaka@outlook.com>
2015-09-27 12:46:11 -0400

    new stuff

---
+++ TidyStart.PowerShell/tools/chocolateyinstall.ps1
@@ -0,0 +1,5 @@
+﻿$packageName = 'TidyStart.powershell'
+$psFile = Join-Path $(Split-Path -parent $MyInvocation.MyCommand.Definition) 'TidyStart.ps1'
+$url = 'https://cdn.rawgit.com/Link-Satonaka/scripts/64480bd653e42ebbcf4f8d78226917b7c377190c/TidyStart.ps1'
+
+Install-ChocolateyPowershellCommand $packageName $psFile $url
---
+++ TidyStart.PowerShell/tools/chocolateyuninstall.ps1
@@ -0,0 +1,4 @@
+﻿# Chocolatey doesn't clean up after its own powershell install function
+If (Test-Path -Path "$ENV:ChocolateyInstall\bin\TidyStart.bat") {
+	Remove-Item -Path "$ENV:ChocolateyInstall\bin\TidyStart.bat"
+}
