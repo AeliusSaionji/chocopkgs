98b09e4a6e99115d954adcd270e3131fe8552280
Link Satonaka <link.satonaka@outlook.com>
2015-09-29 18:22:47 -0400

    vim-x64

---
+++ vim-x64/tools/chocolateyinstall.ps1
@@ -0,0 +1,13 @@
+﻿$packageName = 'vim-x64' # arbitrary name for the package, used in messages
+$fileFullPath = "$ENV:Temp\vim-x64.exe"
+$fileType = 'exe'
+$silentArgs = '/S'
+$version = '{{PackageVersion}}'
+$url = "https://bintray.com/artifact/download/veegee/generic/vim${version}_x64.exe"
+$checksum = '{{Checksum}}'
+$checksumType = 'SHA1'
+
+# Elevation bug, omitted until further notice
+#Install-ChocolateyPackage $packageName $fileType $silentArgs $url -checksum $checksum -checksumType $checksumType
+Get-ChocolateyWebFile $packageName $fileFullPath $url -checksum $checksum -checksumType $checksumType
+Start-Process -FilePath $fileFullPath -ArgumentList $silentArgs -Verb Runas
---
+++ vim-x64/tools/chocolateyuninstall.ps1
@@ -0,0 +1,29 @@
+﻿$packageName = 'vim-x64'
+$registryUninstallerKeyName = 'Vim 7.4'
+$shouldUninstall = $true
+
+$machine_key   = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\$registryUninstallerKeyName"
+
+$file = $machine_key `
+    | ?{ Test-Path $_ } `
+    | Get-ItemProperty `
+    | Select-Object -ExpandProperty UninstallString
+
+if ($file -eq $null -or $file -eq '') {
+    Write-Host "$packageName has already been uninstalled by other means."
+    $shouldUninstall = $false
+}
+
+$installerType = 'EXE' 
+$silentArgs = '/S'
+$validExitCodes = @(0)
+
+#if (!(Test-Path $file)) {
+#    Write-Host "$packageName has already been uninstalled by other means."
+#    $shouldUninstall = $false
+#}
+
+
+if ($shouldUninstall) {
+ Uninstall-ChocolateyPackage -PackageName $packageName -FileType $installerType -SilentArgs $silentArgs -validExitCodes $validExitCodes -File $file
+}
