09c6d28b7d2582a179817c933f3482a275822708
Link Satonaka <link.satonaka@outlook.com>
2015-09-29 19:57:04 -0400

    finalizing

--- vim-x64/tools/chocolateyuninstall.ps1
+++ vim-x64/tools/chocolateyuninstall.ps1
@@ -1,6 +1,7 @@
 ﻿$packageName = 'vim-x64'
 $registryUninstallerKeyName = 'Vim 7.4'
 $shouldUninstall = $true
+$scriptPath = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
 
 $machine_key   = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\$registryUninstallerKeyName"
 
@@ -15,7 +16,7 @@ if ($file -eq $null -or $file -eq '') {
 }
 
 $installerType = 'EXE' 
-$silentArgs = '/S'
+#$silentArgs = '/S'
 $validExitCodes = @(0)
 
 #if (!(Test-Path $file)) {
@@ -25,5 +26,13 @@ $validExitCodes = @(0)
 
 
 if ($shouldUninstall) {
- Uninstall-ChocolateyPackage -PackageName $packageName -FileType $installerType -SilentArgs $silentArgs -validExitCodes $validExitCodes -File $file
+ # Enforce elevation until chocolatey bug is resolved
+ Start-Process powershell -ArgumentList "Uninstall-ChocolateyPackage -PackageName $packageName -FileType $installerType -validExitCodes $validExitCodes -File $file" -Verb RunAs
+ # Uninstaller doesn't seem to work properly, so let's clean up after it
+ # Explorer handle prevents removal, reboot explorer
+ Get-Process | Where { $_.name -eq 'explorer' } | Stop-Process
+ # I hope you don't have any documents open in the editor you're uninstalling...
+ Get-Process | Where { $_.name -eq 'vim'      } | Stop-Process
+ Get-Process | Where { $_.name -eq 'gvim'     } | Stop-Process
+ Start-Process PowerShell -ArgumentList "-File $scriptPath\housekeeping.ps1" -Verb RunAs
 }
