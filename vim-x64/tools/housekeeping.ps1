eaa0d8390cfe25cc4f22c8c612b7ef29f9c92716
Link Satonaka <link.satonaka@outlook.com>
2015-09-29 20:16:26 -0400

    deleted

--- vim-x64/tools/housekeeping.ps1
+++ vim-x64/tools/housekeeping.ps1
@@ -1,5 +1,3 @@
-# Explorer handle prevents removal, reboot explorer
 Remove-Item -Path "$ENV:ProgramFiles\Vim" -Force -Recurse
 Remove-Item -Path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Vim 7.4' -Force -Recurse
 Remove-Item -Path 'HKLM:\SOFTWARE\Vim' -Force -Recurse
-sleep 50
