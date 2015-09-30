810853911d9f5d087642efdecaf8e353359c0c48
Link Satonaka <link.satonaka@outlook.com>
2015-09-30 00:32:49 -0400

    latencymon

---
+++ LatencyMon/tools/chocolateyinstall.ps1
@@ -0,0 +1,6 @@
+﻿$packageName = 'LatencyMon'
+$fileType = 'EXE'
+$silentArgs = '/SP- /VERYSILENT /SUPPRESSMSGBOXES /NORESTART'
+$url = 'http://www.resplendence.com/download/LatencyMon.exe'
+
+Install-ChocolateyPackage $packageName $fileType $silentArgs $url
