8ba7083f0c57dc3add0748c151d8a9050af5c0d3
Link Satonaka <link.satonaka@outlook.com>
2015-12-22 22:28:01 -0500

    fosshub script update

--- HWiNFO.install/tools/chocolateyinstall.ps1
+++ HWiNFO.install/tools/chocolateyinstall.ps1
@@ -1,16 +1,18 @@
 ﻿$PSScriptRoot = Split-Path -Parent $MyInvocation.MyCommand.Definition
-Import-Module (Join-Path $PSScriptRoot 'get-fosshublinks.ps1')
+Import-Module "$PSScriptRoot\Get-UrlFromFosshub.ps1"
 
 $packageName = 'HWiNFO.install'
 $fileType = 'exe'
 $silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART'
 $version = '{{PackageVersion}}'
 $version = $version.Replace(".","")
-$url = Get-FosshubLinks "http://www.fosshub.com/genLink/HWiNFO/hw32_$version.exe"
-$url64 = Get-FosshubLinks "http://www.fosshub.com/genLink/HWiNFO/hw64_$version.exe"
+$url = Get-UrlFromFosshub "http://www.fosshub.com/genLink/HWiNFO/hw32_$version.exe"
+$url64 = Get-UrlFromFosshub "http://www.fosshub.com/genLink/HWiNFO/hw64_$version.exe"
 $checksum = '{{Checksum}}'
 $checksumType = 'SHA1'
 $checksum64 = '{{Checksumx64}}'
 $checksumType64 = 'SHA1'
 
 Install-ChocolateyPackage $packageName $fileType $silentArgs $url $url64 -checksum $checksum -checksumType $checksumType -checksum64 $checksum64 -checksumType64 $checksumType64
+# Kill the process after it starts- there is no way to prevent autostart after install
+Get-Process | Where { $_.name -eq 'HWiNFO64' } | Stop-Process
--- HWiNFO.portable/tools/chocolateyinstall.ps1
+++ HWiNFO.portable/tools/chocolateyinstall.ps1
@@ -1,15 +1,15 @@
 ﻿$PSScriptRoot = Split-Path -Parent $MyInvocation.MyCommand.Definition
-Import-Module (Join-Path $PSScriptRoot 'get-fosshublinks.ps1')
+Import-Module "$PSScriptRoot\Get-UrlFromFosshub.ps1"
 
 $packageName = 'HWiNFO.portable'
 $version = '{{PackageVersion}}'
 $version = $version.Replace(".","")
-$url = Get-FosshubLinks "http://www.fosshub.com/genLink/HWiNFO/hw32_$version.zip"
+$url = Get-UrlFromFosshub "http://www.fosshub.com/genLink/HWiNFO/hw32_$version.zip"
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url64 = Get-FosshubLinks "http://www.fosshub.com/genLink/HWiNFO/hw64_$version.zip"
+$url64 = Get-UrlFromFosshub "http://www.fosshub.com/genLink/HWiNFO/hw64_$version.zip"
 $checksum = '{{Checksum}}'
 $checksumType = 'SHA1'
 $checksum64 = '{{Checksumx64}}'
 $checksumType64 = 'SHA1'
 
-Install-ChocolateyPackage $packageName $url $toolsDir $url64 -checksum $checksum -checksumType $checksumType -checksum64 $checksum64 -checksumType64 $checksumType64
+Install-ChocolateyZipPackage $packageName $url $toolsDir $url64 -checksum $checksum -checksumType $checksumType -checksum64 $checksum64 -checksumType64 $checksumType64
