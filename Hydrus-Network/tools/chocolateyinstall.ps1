fa8c16891abb3329b32054c75846030f05b68c96
Link Satonaka <link.satonaka@outlook.com>
2015-12-26 02:31:56 -0500

    hydrus pkg

---
+++ Hydrus-Network/tools/chocolateyinstall.ps1
@@ -0,0 +1,12 @@
+﻿$packageName = 'Hydrus-Network'
+$fileType = 'exe'
+$silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART'
+$messyversion = '{{PackageVersion}}'
+$version = $messyversion.Remove(3)
+$url = "https://github.com/hydrusnetwork/hydrus/releases/download/v$version/Hydrus.Network.$version.-.Windows.-.Installer.exe"
+
+# Configure the installDir
+New-Item -Path 'HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\Hydrus Network_is1\' -ErrorAction SilentlyContinue
+# I've been told Get-BinRoot is being replaced, not eliminated. I'll just hardcode this for now.
+Set-ItemProperty -Path 'HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\Hydrus Network_is1\' 'Inno Setup: App Path' 'C:\tools\hydrus'
+Install-ChocolateyPackage $packageName $fileType $silentArgs $url
