415f89dc6e957443627b48c9e1c857a2ae66ba5f
Link <link.satonaka@outlook.com>
2015-12-28 01:51:05 -0500

    7zip

---
+++ 7zip.portable/tools/chocolateyinstall.ps1
@@ -0,0 +1,19 @@
+﻿$packageName = '7zip.portable'
+$toolsDir = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
+$versionMinusDots = '{{PackageVersion}}'.Replace(".","")
+$url = "http://www.7-zip.org/a/7z$($versionMinusDots).exe"
+$url64 = "http://www.7-zip.org/a/7z$($versionMinusDots)-x64.exe"
+$extrasUrl = "http://www.7-zip.org/a/7z$($versionMinusDots)-extra.7z"
+
+Install-ChocolateyZipPackage $packageName $url $toolsDir $url64
+Install-ChocolateyZipPackage $packageName $extrasUrl $toolsDir
+
+Remove-Item -Path "$toolsDir\Uninstall.exe"
+
+if (Get-ProcessorBits 32) {
+  # generate ignore for x64\7za.exe
+  New-Item "$toolsDir\x64\7za.exe.ignore" -Type file -Force | Out-Null
+} else {
+  #generate ignore for 7za.exe and let x64 version pick up and shim
+  New-Item "$toolsDir\7za.exe.ignore" -Type file -Force | Out-Null
+}
