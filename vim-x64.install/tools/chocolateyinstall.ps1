63f23ea16dd94180b8a059d70b4c249e0df0ecaa
Link <link.satonaka@outlook.com>
2015-12-28 23:03:16 -0500

    tri package! created meta/portable

---
+++ vim-x64.install/tools/chocolateyinstall.ps1
@@ -0,0 +1,17 @@
+﻿$packageName = 'vim-x64'
+$fileType = 'exe'
+$silentArgs = '/S'
+$version = '{{PackageVersion}}'
+$url = "https://bintray.com/artifact/download/veegee/generic/vim${version}_x64.exe"
+$checksum = '{{Checksum}}'
+$checksumType = 'SHA1'
+
+# An uninstaller that doesn't accept arguments will run if this key exists
+Remove-Item -Path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Vim 7.4' -Force -ErrorAction "SilentlyContinue"
+# Manual uninstall
+# Explorer handle prevents removal, reboot explorer
+Get-Process | Where { $_.name -eq 'explorer'      } | Stop-Process -Force
+Get-Process | Where { $_.name -eq 'FreeCommander' } | Stop-Process -Force
+Remove-Item -Path "$ENV:ProgramFiles\Vim" -Force -Recurse
+
+Install-ChocolateyPackage $packageName $fileType $silentArgs $url -checksum $checksum -checksumType $checksumType
---
+++ vim-x64.portable/tools/chocolateyinstall.ps1
@@ -0,0 +1,19 @@
+﻿$packageName = 'vim-x64.portable'
+$toolsDir = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
+$fileFullPath = Join-Path "$toolsDir" 'vimInstall.exe'
+$version = '{{PackageVersion}}'
+$url = "https://bintray.com/artifact/download/veegee/generic/vim${version}_x64.exe"
+$checksum = '{{Checksum}}'
+$checksumType = 'SHA1'
+
+Get-ChocolateyWebFile $packageName $fileFullPath $url -checksum $checksum -checksumType $checksumType
+
+# Extract using 7z
+$outPath = "-o$toolsDir"
+7z x $outPath $fileFullPath -aoa
+Remove-Item "$fileFullPath"
+Remove-Item "$toolsDir\`$TEMP" -Recurse
+Remove-Item "$toolsDir\`$PLUGINSDIR" -Recurse
+Remove-Item "$toolsDir\vim74\install.exe"
+Remove-Item "$toolsDir\vim74\uninstal.exe"
+Remove-Item "$toolsDir\vim74\uninstall-gui.exe"
