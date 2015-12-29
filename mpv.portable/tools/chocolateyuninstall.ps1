13f11bbfbb1d8251b2cfd8393f8ee356d3f153ee
Link Satonaka <link.satonaka@outlook.com>
2015-12-29 16:30:35 -0500

    remove unnecessary leftover variable

--- mpv.portable/tools/chocolateyuninstall.ps1
+++ mpv.portable/tools/chocolateyuninstall.ps1
@@ -1,3 +1,2 @@
-$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 Remove-Item -Path "$ENV:ChocolateyInstall\bin\mpv.exe"
 Remove-Item -Path "$ENV:ChocolateyInstall\bin\mpv.com"
