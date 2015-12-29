e2e4bbfa07237a259bbb9d40a89f5fc6309ff23a
Link Satonaka <link.satonaka@outlook.com>
2015-12-29 02:16:48 -0500

    straightforward install

---
+++ qutebrowser.install/tools/chocolateyinstall.ps1
@@ -0,0 +1,8 @@
+﻿$packageName = 'qutebrowser.install'
+$installerType = 'msi'
+$silentArgs = '/quiet /qn /norestart'
+$version = '{{PackageVersion}}'
+$url = "https://github.com/The-Compiler/qutebrowser/releases/download/v0.4.1/qutebrowser-$version-win32.msi"
+$url64 = "https://github.com/The-Compiler/qutebrowser/releases/download/v0.4.1/qutebrowser-$version-amd64.msi"
+
+Install-ChocolateyPackage $packageName $installerType $silentArgs $url $url64
