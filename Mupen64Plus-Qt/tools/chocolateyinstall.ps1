101d171fe11b8f659800eea94b797cc11550d4f4
Link <link.satonaka@outlook.com>
2016-01-01 16:08:10 -0500

    finalized package

--- Mupen64Plus-Qt/tools/chocolateyinstall.ps1
+++ Mupen64Plus-Qt/tools/chocolateyinstall.ps1
@@ -1,3 +1,10 @@
-﻿download
-insert into mupen64plus
-create shortcuts
+﻿$packageName = 'Mupen64Plus-Qt'
+$version = '{{PackageVersion}}'
+$url = "https://github.com/dh4/mupen64plus-qt/releases/download/$version/mupen64plus-qt_win_$version.zip"
+$fileFullPath = "$ENV:TEMP\chocolatey\$packageName\$packageName.zip"
+$destination = "$ENV:ChocolateyInstall\lib\mupen64plus\tools"
+
+Get-ChocolateyWebFile $packageName $fileFullPath $url
+Get-ChocolateyUnzip $fileFullPath $destination
+Install-ChocolateyShortcut -shortcutFilePath "$ENV:ALLUSERSPROFILE\Microsoft\Windows\Start Menu\Programs\Mupen64Plus-Qt.lnk" -targetPath "$destination\mupen64plus-qt.exe"
+Install-ChocolateyShortcut -shortcutFilePath "$ENV:APPDATA\Microsoft\Windows\Start Menu\Programs\Mupen64Plus-Qt.lnk" -targetPath "$destination\mupen64plus-qt.exe"
