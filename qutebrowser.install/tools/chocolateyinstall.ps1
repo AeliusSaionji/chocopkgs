76680b2eedda6d62ca18dfbba24e31ecb662d224
Link Satonaka <link.satonaka@outlook.com>
2016-01-01 03:37:33 -0500

    variable fix

--- qutebrowser.install/tools/chocolateyinstall.ps1
+++ qutebrowser.install/tools/chocolateyinstall.ps1
@@ -1,9 +1,9 @@
 ﻿$packageName = 'qutebrowser.install'
 $installerType = 'msi'
-$silentArgs = "/qn /norestart /l*v `"$env:TEMP\chocolatey\$($packageName)\$($packageName).MsiInstall.log`"" # ALLUSERS=1 DISABLEDESKTOPSHORTCUT=1 ADDDESKTOPICON=0 ADDSTARTMENU=0
+$silentArgs = "/qn /norestart /l*v `"$env:TEMP\chocolatey\$($packageName)\$($packageName).MsiInstall.log`""
 $validExitCodes = @(0, 3010, 1641)
 $version = '{{PackageVersion}}'
-$url = "https://github.com/The-Compiler/qutebrowser/releases/download/v0.4.1/qutebrowser-$version-win32.msi"
-$url64 = "https://github.com/The-Compiler/qutebrowser/releases/download/v0.4.1/qutebrowser-$version-amd64.msi"
+$url = "https://github.com/The-Compiler/qutebrowser/releases/download/v$version/qutebrowser-$version-win32.msi"
+$url64 = "https://github.com/The-Compiler/qutebrowser/releases/download/v$version/qutebrowser-$version-amd64.msi"
 
 Install-ChocolateyPackage $packageName $installerType $silentArgs $url $url64 $validExitCodes
