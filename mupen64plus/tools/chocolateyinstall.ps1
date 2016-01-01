87cf6f8cc3a81495746b7a647c145985f17b8866
Link Satonaka <link.satonaka@outlook.com>
2016-01-01 03:38:59 -0500

    new files

---
+++ mupen64plus/tools/chocolateyinstall.ps1
@@ -0,0 +1,6 @@
+﻿$packageName = 'mupen64plus'
+$toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$version     = '{{PackageVersion}}'
+$url         = "https://github.com/mupen64plus/mupen64plus-core/releases/download/$version/mupen64plus-bundle-win32-$version.zip"
+
+Install-ChocolateyZipPackage $packageName $url $toolsDir
