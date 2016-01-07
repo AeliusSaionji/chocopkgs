c745aab6339dbcbc2498231d79184b44703e9229
Link Satonaka <link.satonaka@outlook.com>
2016-01-07 16:14:10 -0500

    use the new template

--- HWiNFO.install/tools/chocolateyuninstall.ps1
+++ HWiNFO.install/tools/chocolateyuninstall.ps1
@@ -1,33 +1,32 @@
-﻿$packageName = 'HWiNFO'
-$shouldUninstall = $true
-
-$machine_key   = 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\HWiNFO64_is1'
-# 32bit version will be here on a 32bit OS
-$machine_key32  = 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\HWiNFO32_is1'
-# If you installed the 32bit version on a 64bit OS you're doing it wrong
-# But I'm covering that eventuality here anyway.
-$machine_key6432 = 'HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\HWiNFO32_is1'
-
-$file = @($machine_key, $machine_key32, $machine_key6432) `
-    | ?{ Test-Path $_ } `
-    | Get-ItemProperty `
-    | Select-Object -ExpandProperty UninstallString
-
-if ($file -eq $null -or $file -eq '') {
-    Write-Host "$packageName has already been uninstalled by other means."
-    $shouldUninstall = $false
-}
-
+﻿$packageName = 'HWiNFO.install'
+$softwareName = 'HWiNFO*'
 $installerType = 'EXE' 
-$silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART'
+$silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-' # Inno Setup
 $validExitCodes = @(0)
 
-# I can't explain why this doesn't work, my powershell is not strong
-#if (!(Test-Path $file)) {
-#    Write-Host "$packageName has already been uninstalled by other means."
-#    $shouldUninstall = $false
-#}
+$uninstalled = $false
+$local_key     = 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*'
+$machine_key   = 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\*'
+$machine_key6432 = 'HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*'
 
-if ($shouldUninstall) {
- Uninstall-ChocolateyPackage -PackageName $packageName -FileType $installerType -SilentArgs $silentArgs -validExitCodes $validExitCodes -File $file
+[array]$key = Get-ItemProperty -Path @($machine_key6432,$machine_key, $local_key) `
+                        -ErrorAction SilentlyContinue `
+         | ? { $_.DisplayName -like "$softwareName" }
+
+if ($key.Count -eq 1) {
+  $key | % { 
+    $file = "$($_.UninstallString)"
+    Uninstall-ChocolateyPackage -PackageName $packageName `
+                                -FileType $installerType `
+                                -SilentArgs "$silentArgs" `
+                                -ValidExitCodes $validExitCodes `
+                                -File "$file"
+  }
+} elseif ($key.Count -eq 0) {
+  Write-Warning "$packageName has already been uninstalled by other means."
+} elseif ($key.Count -gt 1) {
+  Write-Warning "$key.Count matches found!"
+  Write-Warning "To prevent accidental data loss, no programs will be uninstalled."
+  Write-Warning "Please alert package maintainer the following keys were matched:"
+  $key | % {Write-Warning "- $_.DisplayName"}
 }
