51c9f01d9be9e5549929c5bbbb603aa0a0ee4f57
Link Satonaka <link.satonaka@outlook.com>
2016-01-10 10:10:25 -0500

    finalizing

--- Honeyview/tools/chocolateyinstall.ps1
+++ Honeyview/tools/chocolateyinstall.ps1
@@ -1,6 +1,1 @@
-﻿$packageName = 'Honeyview'
-$fileType = 'EXE'
-$silentArgs = '/S'
-$url = 'http://ca-dl.bandisoft.com/honeyview/HONEYVIEW-SETUP.EXE'
-
-Install-ChocolateyPackage $packageName $fileType $silentArgs $url
+﻿#Install-VirtualPackage 'honeyview.portable' 'honeyview.install'
