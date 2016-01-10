dd68f76615486f519bfbbe638b167dce2df5cdca
Link Satonaka <link.satonaka@outlook.com>
2016-01-10 10:22:53 -0500

    32bit silence

--- HWiNFO.install/tools/chocolateyinstall.ps1
+++ HWiNFO.install/tools/chocolateyinstall.ps1
@@ -16,3 +16,4 @@ $checksumType64 = 'SHA1'
 Install-ChocolateyPackage $packageName $fileType $silentArgs $url $url64 -checksum $checksum -checksumType $checksumType -checksum64 $checksum64 -checksumType64 $checksumType64
 # Kill the process after it starts- there is no way to prevent autostart after install
 Get-Process | Where { $_.name -eq 'HWiNFO64' } | Stop-Process
+Get-Process | Where { $_.name -eq 'HWiNFO32' } | Stop-Process
