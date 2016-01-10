ce957b605745f21226da2a344cc87b0f51bee7df
Link Satonaka <link.satonaka@outlook.com>
2016-01-10 12:40:55 -0500

    explorer reboot warning

--- vim-x64.install/tools/chocolateyinstall.ps1
+++ vim-x64.install/tools/chocolateyinstall.ps1
@@ -9,7 +9,7 @@ $checksumType = 'SHA1'
 # An uninstaller that doesn't accept arguments will run if this key exists
 Remove-Item -Path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Vim 7.4' -Force -ErrorAction "SilentlyContinue"
 # Manual uninstall
-# Explorer handle prevents removal, reboot explorer
+Write-Output "Rebooting explorer to release a handle"
 Get-Process | Where { $_.name -eq 'explorer'      } | Stop-Process -Force
 Get-Process | Where { $_.name -eq 'FreeCommander' } | Stop-Process -Force
 Remove-Item -Path "$ENV:ProgramFiles\Vim" -Force -Recurse
