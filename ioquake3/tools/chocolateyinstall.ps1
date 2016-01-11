0572c8e01761f4b333a8552852d888a83380af03
Link <link.satonaka@outlook.com>
2016-01-10 23:51:38 -0500

    find steam/gog quake3 pak0

--- ioquake3/tools/chocolateyinstall.ps1
+++ ioquake3/tools/chocolateyinstall.ps1
@@ -12,4 +12,36 @@ $validExitCodes= @(0)
 Install-ChocolateyPackage $packageName $fileType $silentArgs $url -validExitCodes $validExitCodes -checksum $checksum -checksumType $checksumType
 Install-ChocolateyPackage $packageName $fileType $silentArgs $urlDataInstaller -validExitCodes $validExitCodes -checksum $checksumDataInstaller -checksumType $checksumType
 
-# test for steam, copy pak0 over
+# Find ioquake3 folder
+$local_key     = 'HKCU:\Software\*'
+$machine_key   = 'HKLM:\SOFTWARE\*'
+$machine_key6432 = 'HKLM:\SOFTWARE\Wow6432Node\*'
+[array]$instDirKey = Get-ItemProperty -Path @($machine_key6432,$machine_key, $local_key) `
+                        -ErrorAction SilentlyContinue `
+         | ? { $_.Install_Dir -like "*ioquake3" }
+$baseq3Dir = Join-Path $instDirKey.Install_Dir "baseq3"
+$missionDir = Join-Path $instDirKey.Install_Dir "missionpack"
+
+# Find pak0 from existing Steam quake3 installation
+$local_key     = 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*'
+$machine_key   = 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\*'
+$machine_key6432 = 'HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*'
+[array]$steamDir = Get-ItemProperty -Path @($machine_key6432,$machine_key, $local_key) `
+                        -ErrorAction SilentlyContinue `
+         | ? { $_.DisplayName -like "Quake III Arena" }
+$pakPath = Join-Path $steamDir.InstallLocation "baseq3\pak0.pk3"
+$pakTAPath = Join-Path $steamDir.InstallLocation "missionpack\pak0.pk3"
+Copy-Item $pakPath $baseq3Dir -ErrorAction SilentlyContinue
+Copy-Item $pakTAPath $missionDir -ErrorAction SilentlyContinue
+
+# Find pak0 from existing GOG quake3 installation
+$local_key     = 'HKCU:\Software\GOG.com\Games\*'
+$machine_key   = 'HKLM:\SOFTWARE\GOG.com\Games\*'
+$machine_key6432 = 'HKLM:\SOFTWARE\Wow6432Node\GOG.com\Games\*'
+[array]$gogDir = Get-ItemProperty -Path @($machine_key6432,$machine_key, $local_key) `
+                        -ErrorAction SilentlyContinue `
+         | ? { $_.GAMENAME -like "Quake III Gold" }
+$pakPath = Join-Path $gogDir.PATH "baseq3\pak0.pk3"
+$pakTAPath = Join-Path $gogDir.PATH "missionpack\pak0.pk3"
+Copy-Item $pakPath $baseq3Dir -ErrorAction SilentlyContinue
+Copy-Item $pakTAPath $missionDir -ErrorAction SilentlyContinue
