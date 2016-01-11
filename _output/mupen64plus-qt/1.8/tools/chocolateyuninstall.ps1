df42140968bb83320e58ddd8832891daa2d9c3b1
Link <link.satonaka@outlook.com>
2016-01-11 10:42:09 -0500

    output

---
+++ _output/mupen64plus-qt/1.8/tools/chocolateyuninstall.ps1
@@ -0,0 +1,4 @@
+﻿Remove-Item -Path "$ENV:ChocolateyInstall\lib\mupen64plus\tools\mupen64plus-qt.exe"
+Remove-Item -Path "$ENV:ChocolateyInstall\lib\mupen64plus\tools\README.txt"
+Remove-Item -Path "$ENV:ALLUSERSPROFILE\Microsoft\Windows\Start Menu\Programs\Mupen64Plus-Qt.lnk" -ErrorAction SilentlyContinue
+Remove-Item -Path "$ENV:APPDATA\Microsoft\Windows\Start Menu\Programs\Mupen64Plus-Qt.lnk" -ErrorAction SilentlyContinue
