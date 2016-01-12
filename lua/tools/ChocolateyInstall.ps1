712c246db8ca418a9bf7e2afb0680a36ac4f302c
Link <link.satonaka@outlook.com>
2016-01-12 18:03:05 -0500

    luaforwindows

---
+++ lua/tools/ChocolateyInstall.ps1
@@ -0,0 +1,1 @@
+Install-ChocolateyPackage 'lua' 'exe' '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-' 'https://github.com/rjpcomputing/luaforwindows/releases/download/v5.1.4-50/LuaForWindows_v5.1.4-50.exe'
