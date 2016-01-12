f65c82fe7c3429a54c2ddef5b62772ce329dbf78
Link <link.satonaka@outlook.com>
2016-01-11 20:09:03 -0500

    now functional

--- ioquake3/tools/chocolateyinstall.ps1
+++ ioquake3/tools/chocolateyinstall.ps1
@@ -16,21 +16,23 @@ Install-ChocolateyPackage $packageName $fileType $silentArgs $urlDataInstaller -
 $local_key     = 'HKCU:\Software\*'
 $machine_key   = 'HKLM:\SOFTWARE\*'
 $machine_key6432 = 'HKLM:\SOFTWARE\Wow6432Node\*'
-[array]$instDirKey = Get-ItemProperty -Path @($machine_key6432,$machine_key, $local_key) `
-                        -ErrorAction SilentlyContinue `
-         | ? { $_.Install_Dir -like "*ioquake3" }
-$baseq3Dir = Join-Path $instDirKey.Install_Dir "baseq3"
-$missionDir = Join-Path $instDirKey.Install_Dir "missionpack"
+Get-ItemProperty -Path @($machine_key6432,$machine_key, $local_key) `
+	-ErrorAction SilentlyContinue `
+	| ? { $_.PSChildName -like "ioquake3" } `
+	| ForEach-Object { `
+	$baseq3Dir = Join-Path $_.Install_Dir "baseq3" ; `
+	$missionDir = Join-Path $_.Install_Dir "missionpack" }
 
 # Find pak0 from existing Steam quake3 installation
 $local_key_steam     = 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*'
 $machine_key_steam   = 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\*'
 $machine_key6432_steam = 'HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*'
-[array]$steamDir = Get-ItemProperty -Path @($machine_key6432_steam,$machine_key_steam, $local_key_steam) `
-                        -ErrorAction SilentlyContinue `
-         | ? { $_.DisplayName -like "Quake III Arena" }
-$pakPath = Join-Path $steamDir.InstallLocation "baseq3\pak0.pk3"
-$pakTAPath = Join-Path $steamDir.InstallLocation "missionpack\pak0.pk3"
+Get-ItemProperty -Path @($machine_key6432_steam,$machine_key_steam, $local_key_steam) `
+	-ErrorAction SilentlyContinue `
+	| ? { $_.DisplayName -like "Quake III Arena" } `
+	| ForEach-Object { if (($_.InstallLocation -ne $null) -and ($_.InstallLocation -ne '')) { `
+	$pakPath = Join-Path $_.InstallLocation "baseq3\pak0.pk3" ; ` 
+	$pakTAPath = Join-Path $_.InstallLocation "missionpack\pak0.pk3" }}
 Copy-Item $pakPath $baseq3Dir -ErrorAction SilentlyContinue
 Copy-Item $pakTAPath $missionDir -ErrorAction SilentlyContinue
 
@@ -38,10 +40,12 @@ Copy-Item $pakTAPath $missionDir -ErrorAction SilentlyContinue
 $local_key_gog     = 'HKCU:\Software\GOG.com\Games\*'
 $machine_key_gog   = 'HKLM:\SOFTWARE\GOG.com\Games\*'
 $machine_key6432_gog = 'HKLM:\SOFTWARE\Wow6432Node\GOG.com\Games\*'
-[array]$gogDir = Get-ItemProperty -Path @($machine_key6432_gog,$machine_key_gog, $local_key_gog) `
-                        -ErrorAction SilentlyContinue `
-         | ? { $_.GAMENAME -like "Quake III Gold" }
-$pakPath = Join-Path $gogDir.PATH "baseq3\pak0.pk3"
-$pakTAPath = Join-Path $gogDir.PATH "missionpack\pak0.pk3"
+Get-ItemProperty -Path @($machine_key6432_gog,$machine_key_gog, $local_key_gog) `
+	-ErrorAction SilentlyContinue `
+	| ? { $_.GAMENAME -like "Quake III Gold" } `
+	| ForEach-Object { if (($_.PATH -ne $null) -and ($_.PATH -ne '')) { `
+	$pakPath = Join-Path $_.PATH "baseq3\pak0.pk3" ;
+	$pakTAPath = Join-Path $_.PATH "missionpack\pak0.pk3" }}
 Copy-Item $pakPath $baseq3Dir -ErrorAction SilentlyContinue
 Copy-Item $pakTAPath $missionDir -ErrorAction SilentlyContinue
+
