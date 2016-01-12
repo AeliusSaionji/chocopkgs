e856bcb114b205b3bf977e686556a60009cb8b40
Link <link.satonaka@outlook.com>
2016-01-12 13:03:25 -0500

    initial commit

---
+++ ScpToolkit/tools/chocolateyinstall.ps1
@@ -0,0 +1,23 @@
+﻿$packageName= 'ScpToolkit' # arbitrary name for the package, used in messages
+$url        = '' # download url
+$fileType      = 'EXE_MSI_OR_MSU' #only one of these: exe, msi, msu
+
+  #MSI
+  silentArgs    = "/qn /norestart /l*v `"$env:TEMP\chocolatey\$($packageName)\$($packageName).MsiInstall.log`"" # ALLUSERS=1 DISABLEDESKTOPSHORTCUT=1 ADDDESKTOPICON=0 ADDSTARTMENU=0
+  validExitCodes= @(0, 3010, 1641)
+  #OTHERS
+  # Uncomment matching EXE type (sorted by most to least common)
+  #silentArgs   = '/S'           # NSIS
+  #silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-' # Inno Setup
+  #silentArgs   = '/s'           # InstallShield
+  #silentArgs   = '/s /v"/qn"' # InstallShield with MSI
+  #silentArgs   = '/s'           # Wise InstallMaster
+  #silentArgs   = '-s'           # Squirrel
+  #silentArgs   = '-q'           # Install4j
+  #silentArgs   = '-s -u'        # Ghost
+  # Note that some installers, in addition to the silentArgs above, may also need assistance of AHK to achieve silence.
+  #silentArgs   = ''             # none; make silent with input macro script like AutoHotKey (AHK)
+                                 #       https://chocolatey.org/packages/autohotkey.portable
+  #validExitCodes= @(0) #please insert other valid exit codes here
+
+Install-ChocolateyPackage @packageArgs
