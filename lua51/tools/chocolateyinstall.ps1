e278df6cd58b7eef6b94340b4afd046b2a9a6a5e
Link <link.satonaka@outlook.com>
2016-01-12 00:59:15 -0500

    lua

---
+++ lua51/tools/chocolateyinstall.ps1
@@ -0,0 +1,11 @@
+﻿$packageName = 'lua53'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'http://sourceforge.net/projects/luabinaries/files/5.1.5/Tools%20Executables/lua5_1_5_Win32_bin.zip/download'
+$url64      = 'http://sourceforge.net/projects/luabinaries/files/5.1.5/Tools%20Executables/lua5_1_5_Win64_bin.zip/download'
+
+Install-ChocolateyZipPackage $packageName $url $toolsDir $url64
+
+# Remove hardlink before attempting to recreate it
+Remove-Item -Path "$ENV:ChocolateyInstall\bin\lua51.dll" -ErrorAction SilentlyContinue
+# Get the dll on PATH
+Start-Process cmd.exe -ArgumentList "/C MKLINK /H $ENV:ChocolateyInstall\bin\lua51.dll $toolsDir\lua51.dll"
---
+++ lua53/tools/chocolateyinstall.ps1
@@ -0,0 +1,11 @@
+﻿$packageName = 'lua53'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'http://sourceforge.net/projects/luabinaries/files/5.3.2/Tools%20Executables/lua-5.3.2_Win32_bin.zip/download'
+$url64      = 'http://sourceforge.net/projects/luabinaries/files/5.3.2/Tools%20Executables/lua-5.3.2_Win64_bin.zip/download'
+
+Install-ChocolateyZipPackage $packageName $url $toolsDir $url64
+
+# Remove hardlink before attempting to recreate it
+Remove-Item -Path "$ENV:ChocolateyInstall\bin\lua53.dll" -ErrorAction SilentlyContinue
+# Get the dll on PATH
+Start-Process cmd.exe -ArgumentList "/C MKLINK /H $ENV:ChocolateyInstall\bin\lua53.dll $toolsDir\lua53.dll"
