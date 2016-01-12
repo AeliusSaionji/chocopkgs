1be699d4447761cbb1573ec5741cf9eb6d9765f1
Link <link.satonaka@outlook.com>
2016-01-12 01:56:16 -0500

    wrong packagename, oops

--- lua51/tools/chocolateyinstall.ps1
+++ lua51/tools/chocolateyinstall.ps1
@@ -1,4 +1,4 @@
-﻿$packageName = 'lua53'
+﻿$packageName = 'lua51'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url        = 'http://sourceforge.net/projects/luabinaries/files/5.1.5/Tools%20Executables/lua5_1_5_Win32_bin.zip/download'
 $url64      = 'http://sourceforge.net/projects/luabinaries/files/5.1.5/Tools%20Executables/lua5_1_5_Win64_bin.zip/download'
