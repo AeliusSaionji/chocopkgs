87394687a9b31f22fe501833fc92581787cf283b
Link <link.satonaka@outlook.com>
2016-01-13 14:26:41 -0500

    found this in the installer config

--- GeForce-Experience/tools/chocolateyInstall.ps1
+++ GeForce-Experience/tools/chocolateyInstall.ps1
@@ -2,7 +2,7 @@
 $installerType = 'EXE'
 $version = '{{PackageVersion}}'
 $url = "http://us.download.nvidia.com/GFE/GFEClient/$version/GeForce_Experience_v$version.exe"
-$silentArgs = '/s'
+$silentArgs = '/s /noreboot'
 $validExitCodes = @(0)
 
 Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes $validExitCodes
