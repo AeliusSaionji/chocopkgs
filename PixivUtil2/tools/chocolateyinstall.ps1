46e4747132d2554a3218f48b49b82fdd0e94cb69
Link <link.satonaka@outlook.com>
2016-01-13 20:40:49 -0500

    pixivutil2

---
+++ PixivUtil2/tools/chocolateyinstall.ps1
@@ -0,0 +1,12 @@
+﻿$packageName = 'PixivUtil2'
+$version = '{{PackageVersion}}'
+$versionWithoutDots = $version.Replace('.','')
+$unzipLocation = Join-Path "$(Get-BinRoot)" "$packageName"
+$url = "https://github.com/Nandaka/PixivUtil2/releases/download/v$versionWithoutDots/pixivutil$versionWithoutDots.7z"
+
+Install-ChocolateyZipPackage $packageName $url $unzipLocation
+
+$commProgs = [environment]::getfolderpath('CommonPrograms')
+$userProgs = [environment]::getfolderpath('Programs')
+Install-ChocolateyShortcut -shortcutFilePath "$commProgs\PixivUtil2.lnk" -targetPath "$unzipLocation\PixivUtil2.exe" -WorkingDirectory "$unzipLocation"
+Install-ChocolateyShortcut -shortcutFilePath "$userProgs\PixivUtil2.lnk" -targetPath "$unzipLocation\PixivUtil2.exe" -WorkingDirectory "$unzipLocation"
---
+++ PixivUtil2/tools/chocolateyuninstall.ps1
@@ -0,0 +1,4 @@
+﻿$commProgs = [environment]::getfolderpath('CommonPrograms')
+$userProgs = [environment]::getfolderpath('Programs')
+Remove-Item "$commProgs\PixivUtil2.lnk"
+Remove-Item "$userProgs\PixivUtil2.lnk"
