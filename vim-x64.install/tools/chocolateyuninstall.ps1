a88b18ba6abf52ac897a3c1c5a3b0b825c450968
Link <link.satonaka@outlook.com>
2016-01-14 22:53:03 -0500

    mirroring

--- vim-x64.install/tools/chocolateyuninstall.ps1
+++ vim-x64.install/tools/chocolateyuninstall.ps1
@@ -1,36 +1,38 @@
-﻿$packageName = 'vim-x64'
-$registryUninstallerKeyName = 'Vim 7.4'
-$shouldUninstall = $true
-
-$machine_key   = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\$registryUninstallerKeyName"
-
-$file = $machine_key `
-    | ?{ Test-Path $_ } `
-    | Get-ItemProperty `
-    | Select-Object -ExpandProperty UninstallString
-
-if ($file -eq $null -or $file -eq '') {
-    Write-Host "$packageName has already been uninstalled by other means."
-    $shouldUninstall = $false
-}
-
+﻿$packageName = 'vim-x64.install'
+$softwareName = 'Vim*'
 $installerType = 'EXE' 
-$silentArgs = '/S'
+$silentArgs = '/S' # NSIS
 $validExitCodes = @(0)
+$uninstalled = $false
+$local_key = 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*'
+$machine_key = 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\*'
+$machine_key6432 = 'HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*'
 
-#if (!(Test-Path $file)) {
-#    Write-Host "$packageName has already been uninstalled by other means."
-#    $shouldUninstall = $false
-#}
+[array]$key = Get-ItemProperty -Path @($machine_key6432,$machine_key, $local_key) `
+                        -ErrorAction SilentlyContinue `
+         | ? { $_.DisplayName -like "$softwareName" }
 
+if ($key.Count -eq 1) {
+  $key | % { 
+    $file = "$($_.UninstallString)"
+    Uninstall-ChocolateyPackage -PackageName $packageName `
+                                -FileType $installerType `
+                                -SilentArgs "$silentArgs" `
+                                -ValidExitCodes $validExitCodes `
+                                -File "$file"
+  }
+  # Since the uninstaller doesn't actually work... we have to manually remove vim-x64
+  Write-Output "Rebooting explorer to release a handle"
+  Get-Process | Where { $_.name -eq 'explorer'      } | Stop-Process -Force
+  Get-Process | Where { $_.name -eq 'FreeCommander' } | Stop-Process -Force
+  Remove-Item -Path "$ENV:ProgramFiles\Vim" -Force -Recurse -ErrorAction Stop
+  Remove-Item -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Vim 7.4" -ErrorAction SilentlyContinue
 
-if ($shouldUninstall) {
- Uninstall-ChocolateyPackage -PackageName $packageName -FileType $installerType -SilentArgs $silentArgs -validExitCodes $validExitCodes -File $file
- # Uninstaller doesn't seem to work properly, so let's clean up after it
- # Explorer handle prevents removal, reboot explorer/other file browsers
- Get-Process | Where { $_.name -eq 'explorer'      } | Stop-Process -Force
- Get-Process | Where { $_.name -eq 'FreeCommander' } | Stop-Process -Force
- Remove-Item -Path "$ENV:ProgramFiles\Vim" -Force -Recurse
- Remove-Item -Path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Vim 7.4' -Force -Recurse
- Remove-Item -Path 'HKLM:\SOFTWARE\Vim' -Force -Recurse
+} elseif ($key.Count -eq 0) {
+  Write-Warning "$packageName has already been uninstalled by other means."
+} elseif ($key.Count -gt 1) {
+  Write-Warning "$key.Count matches found!"
+  Write-Warning "To prevent accidental data loss, no programs will be uninstalled."
+  Write-Warning "Please alert package maintainer the following keys were matched:"
+  $key | % {Write-Warning "- $_.DisplayName"}
 }
