4af390c6fcb643f6013ed1e0c7700c9cb0d87d23
Link <link.satonaka@outlook.com>
2016-01-19 11:20:38 -0500

    output sync

---
+++ _output/FreeCommander-XE/2015.685/tools/chocolateyInstall.ps1
@@ -0,0 +1,11 @@
+﻿$packageName = 'FreeCommander XE'
+$toolsDir = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
+$installerType = 'EXE'
+$url = 'http://freecommander.com/downloads/FreeCommanderXE-32-public_setup.zip'
+$silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-' # Inno Setup
+$validExitCodes = @(0)
+
+Install-ChocolateyZipPackage $packageName $url $toolsDir
+$file = Join-Path "$toolsDir" "$(Get-ChildItem $toolsDir -Include '*.exe' -Name)"
+Install-ChocolateyInstallPackage $packageName $installerType $silentArgs $file
+Remove-Item -Path $file
---
+++ _output/qutebrowser.install/0.5.1/tools/chocolateyinstall.ps1
@@ -0,0 +1,9 @@
+﻿$packageName = 'qutebrowser.install'
+$installerType = 'msi'
+$silentArgs = "/qn /norestart /l*v `"$env:TEMP\chocolatey\$($packageName)\$($packageName).MsiInstall.log`""
+$validExitCodes = @(0, 3010, 1641)
+$version = '0.5.1'
+$url = "https://github.com/The-Compiler/qutebrowser/releases/download/v$version/qutebrowser-$version-win32.msi"
+$url64 = "https://github.com/The-Compiler/qutebrowser/releases/download/v$version/qutebrowser-$version-amd64.msi"
+
+Install-ChocolateyPackage $packageName $installerType $silentArgs $url $url64 $validExitCodes
