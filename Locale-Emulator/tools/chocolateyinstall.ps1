b5f0f592dfa889a911c5cf6b4a48a2cb5481e436
Link <link.satonaka@outlook.com>
2016-01-20 01:33:33 -0500

    locale emulator initial commit

---
+++ Locale-Emulator/tools/chocolateyinstall.ps1
@@ -0,0 +1,6 @@
+﻿$packageName = 'Locale-Emulator'
+$version = '{{PackageVersion}}'
+$url = "https://github.com/xupefei/Locale-Emulator/releases/download/v${version}/Locale.Emulator.${version}.zip"
+$destination = Join-Path "$(Get-BinRoot)" "$packageName"
+
+Install-ChocolateyZipPackage $packageName $url $destination
