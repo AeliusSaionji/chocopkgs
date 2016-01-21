0b35482d01463ab550487a9402c4fe544b49e471
Link <link.satonaka@outlook.com>
2016-01-21 10:27:41 -0500

    sync

---
+++ _output/Hydrus-Network/191.0/tools/chocolateyinstall.ps1
@@ -0,0 +1,12 @@
+﻿$packageName = 'Hydrus-Network'
+$fileType = 'exe'
+$silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART'
+$messyversion = '191.0'
+$version = $messyversion.Remove(3)
+$url = "https://github.com/hydrusnetwork/hydrus/releases/download/v$version/Hydrus.Network.$version.-.Windows.-.Installer.exe"
+
+# Configure the installDir
+New-Item -Path 'HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\Hydrus Network_is1\' -ErrorAction SilentlyContinue
+$destination = Join-Path "$(Get-BinRoot)" "hydrus"
+Set-ItemProperty -Path 'HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\Hydrus Network_is1\' 'Inno Setup: App Path' "$destination"
+Install-ChocolateyPackage $packageName $fileType $silentArgs $url
