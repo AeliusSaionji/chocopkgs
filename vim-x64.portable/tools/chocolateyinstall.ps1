7cba30e9223214db7d51e2197bba878f4ff70dc5
Link <link.satonaka@outlook.com>
2016-02-09 18:44:08 -0500

    if they want integration, they should use .install

--- vim-x64.portable/tools/chocolateyinstall.ps1
+++ vim-x64.portable/tools/chocolateyinstall.ps1
@@ -3,8 +3,4 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $destDir = Join-Path $toolsDir "vim74"
 $url = "http://tuxproject.de/projects/vim/complete-x64.7z"
 
-Write-Output "Rebooting explorer to release a handle"
-Get-Process | Where { $_.name -eq 'explorer'      } | Stop-Process -Force
-Get-Process | Where { $_.name -eq 'FreeCommander' } | Stop-Process -Force
-
 Install-ChocolateyZipPackage $packageName $url $destDir
--- vim-x64.portable/tools/chocolateyuninstall.ps1
+++
@@ -1,4 +0,0 @@
-﻿$packageName = 'vim-x64.portable'
-Write-Output "Rebooting explorer to release a handle"
-Get-Process | Where { $_.name -eq 'explorer'      } | Stop-Process -Force
-Get-Process | Where { $_.name -eq 'FreeCommander' } | Stop-Process -Force
