3a8fd756360bf0130849bcff5d5727bc5b8fcd98
Link <link.satonaka@outlook.com>
2016-02-09 01:40:24 -0500

    sync

---
+++ _output/vim-x64.portable/7.4.1290/tools/chocolateyuninstall.ps1
@@ -0,0 +1,4 @@
+﻿$packageName = 'vim-x64.portable'
+Write-Output "Rebooting explorer to release a handle"
+Get-Process | Where { $_.name -eq 'explorer'      } | Stop-Process -Force
+Get-Process | Where { $_.name -eq 'FreeCommander' } | Stop-Process -Force
