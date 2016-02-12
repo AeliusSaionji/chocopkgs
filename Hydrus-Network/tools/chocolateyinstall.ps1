ed976c7f0bb81dfe9acba8b5c455e56a49a52794
Link <link.satonaka@outlook.com>
2016-02-12 00:15:50 -0500

    trick chocolatey into showing a different prompt

--- Hydrus-Network/tools/chocolateyinstall.ps1
+++ Hydrus-Network/tools/chocolateyinstall.ps1
@@ -4,9 +4,10 @@ $silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART'
 $messyversion = '{{PackageVersion}}'
 $version = $messyversion.Remove(3)
 $url = "https://github.com/hydrusnetwork/hydrus/releases/download/v$version/Hydrus.Network.$version.-.Windows.-.Installer.exe"
+$url64 = $url
 
 # Configure the installDir
 New-Item -Path 'HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\Hydrus Network_is1\' -ErrorAction SilentlyContinue
 $destination = Join-Path "$(Get-BinRoot)" "hydrus"
 Set-ItemProperty -Path 'HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\Hydrus Network_is1\' 'Inno Setup: App Path' "$destination"
-Install-ChocolateyPackage $packageName $fileType $silentArgs $url
+Install-ChocolateyPackage $packageName $fileType $silentArgs $url $url64
