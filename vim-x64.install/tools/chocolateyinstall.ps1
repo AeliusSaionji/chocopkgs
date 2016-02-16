743c38b28edbb0295d0fc2379fc49dccbad483ed
Link <link.satonaka@outlook.com>
2016-02-16 18:46:42 -0500

    dont reboot explorer

--- vim-x64.install/tools/chocolateyinstall.ps1
+++ vim-x64.install/tools/chocolateyinstall.ps1
@@ -1,22 +1,46 @@
 ﻿$packageName = 'vim-x64.install'
+$tempFolder = "$ENV:TEMP\chocolatey\vim-x64.install"
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$destDir = Join-Path $toolsDir "vim74"
 $url = "http://tuxproject.de/projects/vim/complete-x64.7z"
+$url64 = $url
 
-# Remove traces of Veege's Build
-if ( (Get-ItemProperty -ErrorAction SilentlyContinue 'HKLM:\software\microsoft\windows\currentversion\uninstall\Vim 7.4\').UninstallString -eq "$ENV:ProgramFiles\Vim\vim74\uninstall-gui.exe" ) {
+function Move-LockedFile { 
+    param($path, $destination)
+
+    $path = (Resolve-Path $path).Path 
+    $destination = $executionContext.SessionState.Path.GetUnresolvedProviderPathFromPSPath($destination)
+
+    $MOVEFILE_DELAY_UNTIL_REBOOT = 0x00000004
+
+    $memberDefinition = @’ 
+    [DllImport("kernel32.dll", SetLastError=true, CharSet=CharSet.Auto)] 
+    public static extern bool MoveFileEx(string lpExistingFileName, string lpNewFileName, 
+       int dwFlags); 
+‘@
+
+    $type = Add-Type -Name MoveFileUtils -MemberDefinition $memberDefinition -PassThru 
+    $type::MoveFileEx($path, $destination, $MOVEFILE_DELAY_UNTIL_REBOOT) 
+}
+
+# Remove traces of Veegee's Build
+if ( (Get-ItemProperty -ErrorAction SilentlyContinue 'HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\Vim 7.4\').UninstallString -eq "$ENV:ProgramFiles\Vim\vim74\uninstall-gui.exe" ) {
  Write-Output "New build installs to ChocolateyLib. Removing from Program Files."
  Remove-Item -Path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Vim 7.4' -ErrorAction SilentlyContinue
  Remove-Item -Path "HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\Vim_x64" -ErrorAction SilentlyContinue
- Write-Output "Rebooting explorer to release a handle"
- Get-Process | Where { $_.name -eq 'explorer'      } | Stop-Process -Force
- Get-Process | Where { $_.name -eq 'FreeCommander' } | Stop-Process -Force
- Remove-Item -Path "$ENV:ProgramFiles\Vim" -Force -Recurse
+ Move-LockedFile "$ENV:ProgramFiles\Vim" "$ENV:TEMP\veegeevim"
 }
 
-Write-Output "Rebooting explorer to release a handle"
-Get-Process | Where { $_.name -eq 'explorer'      } | Stop-Process -Force
-Get-Process | Where { $_.name -eq 'FreeCommander' } | Stop-Process -Force
+# Manually download and extract to temp
+Get-ChocolateyWebFile $packageName $(Join-Path $tempFolder "vim.7z") $url $url64
+Get-ChocolateyUnzip $(Join-Path $tempFolder "vim.7z") $(Join-Path $tempFolder "vim74")
+# Remove this dll
+Move-Item -Force $(Join-Path $tempFolder "vim74\GVimExt\gvimext.dll") $tempFolder
+Move-Item -Force $(Join-Path $tempFolder "vim74\*") $(Join-Path $toolsDir "vim74")
+# Fix file permissions
+Start-Process icacls -ArgumentList "$toolsdir /reset /T /C /Q"
+# Move the dll on reboot
+Move-LockedFile $(Join-Path $tempFolder "gvimext.dll") $(Join-Path $toolsDir "vim74\GVimExt\gvimext.dll")
+# See if the dll can go there now, let it fail if it can't
+Copy-Item -Force $(Join-Path $tempFolder "gvimext.dll") $(Join-Path $toolsDir "vim74\GVimExt") -ErrorAction SilentlyContinue
 
-Install-ChocolateyZipPackage $packageName $url $destDir
-Start-ChocolateyProcessAsAdmin "-create-batfiles evim view gview vimdiff gvimdiff -install-popup -install-openwith -add-start-menu" "$destDir\install.exe" -validExitCodes '0'
+Start-ChocolateyProcessAsAdmin "-create-batfiles evim view gview vimdiff gvimdiff -install-popup -install-openwith -add-start-menu" "$toolsDir\vim74\install.exe" -validExitCodes '0'
