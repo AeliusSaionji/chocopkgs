508dc6bd771fe51eb4ffd6f66e58589bc8452840
Link <link.satonaka@outlook.com>
2016-02-16 21:49:42 -0500

    tricks

--- Honeyview.install/tools/chocolateyinstall.ps1
+++ Honeyview.install/tools/chocolateyinstall.ps1
@@ -2,5 +2,6 @@
 $fileType = 'EXE'
 $silentArgs = '/S'
 $url = 'http://ca-dl.bandisoft.com/honeyview/HONEYVIEW-SETUP.EXE'
+$url64 = $url
 
-Install-ChocolateyPackage $packageName $fileType $silentArgs $url
+Install-ChocolateyPackage $packageName $fileType $silentArgs $url $url64
