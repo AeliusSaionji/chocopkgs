e8b25463f63d8a86a3169fdda06457e6d85d6245
Link <link.satonaka@outlook.com>
2016-02-16 21:49:24 -0500

    now with bats

--- vim-x64.portable/tools/chocolateyinstall.ps1
+++ vim-x64.portable/tools/chocolateyinstall.ps1
@@ -4,4 +4,6 @@ $destDir = Join-Path $toolsDir "vim74"
 $url = "http://tuxproject.de/projects/vim/complete-x64.7z"
 $url64 = $url
 
+Get-ChildItem "$destDir\*.bat" | %{ Install-BinFile -Name $_.BaseName -Path $_ }
 Install-ChocolateyZipPackage $packageName $url $destDir $url64
+Start-Process "$destDir\install.exe" -ArgumentList "-add-start-menu" -WorkingDirectory "$destDir" -Wait
