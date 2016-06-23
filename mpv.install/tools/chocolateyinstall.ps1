da722c805cca485d06290a91cd2da9f1c2ea7b18
Link <link.satonaka@outlook.com>
2016-06-22 22:40:25 -0400

    chocolatey is going to require .NET4.5 soon anyway...

--- mpv.install/tools/chocolateyinstall.ps1
+++ mpv.install/tools/chocolateyinstall.ps1
@@ -6,17 +6,10 @@ $url = "https://mpv.srsfckn.biz/mpv-i686-$version.7z"
 $url64 = "https://mpv.srsfckn.biz/mpv-x86_64-$version.7z"
 $urlAssoc = 'https://github.com/rossy/mpv-install/archive/master.zip'
 
-# Use BITS until https://github.com/chocolatey/choco/pull/459
-# Find bitness for download
-$FileFullPath = "$ENV:TEMP\chocolatey\mpv.install\mpv-i686-$version.7z"
-if $(Get-ProcessorBits) = '64' {
-	$url = $url64
-	$FileFullPath = "$ENV:TEMP\chocolatey\mpv.install\mpv-x86_64-$version.7z"
-}
-Start-BitsTransfer -Source $url -Destination "$ENV:TEMP\chocolatey\mpv.install\"
-#Install-ChocolateyZipPackage $packageName $url $toolsDir $url64
-Get-ChocolateyUnzip -FileFullPath $FileFullPath -Destination $toolsDir -PackageName $packageName
+# Enable TLS 1.2 manually. Someone remind me to remove this bit when Chocolatey 0.9.11 is released
+[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]'Tls12'
 
+Install-ChocolateyZipPackage $packageName $url $toolsDir $url64
 Install-ChocolateyZipPackage "associations" $urlAssoc $toolsDir
 Move-Item "$toolsDir\mpv-install-master\*" $toolsDir -Force
 Remove-Item "$toolsDir\mpv-install-master"
--- mpv.portable/tools/chocolateyinstall.ps1
+++ mpv.portable/tools/chocolateyinstall.ps1
@@ -5,16 +5,10 @@ $version = $version.Replace(".","")
 $url = "https://mpv.srsfckn.biz/mpv-i686-$version.7z"
 $url64 = "https://mpv.srsfckn.biz/mpv-x86_64-$version.7z"
 
-# Use BITS until https://github.com/chocolatey/choco/pull/459
-# Find bitness for download
-$FileFullPath = "$ENV:TEMP\chocolatey\mpv.portable\mpv-i686-$version.7z"
-if $(Get-ProcessorBits) = '64' {
-	$url = $url64
-	$FileFullPath = "$ENV:TEMP\chocolatey\mpv.portable\mpv-x86_64-$version.7z"
-}
-Start-BitsTransfer -Source $url -Destination "$ENV:TEMP\chocolatey\mpv.portable\"
-#Install-ChocolateyZipPackage $packageName $url $toolsDir $url64
-Get-ChocolateyUnzip -FileFullPath $FileFullPath -Destination $toolsDir -PackageName $packageName
+# Enable TLS 1.2 manually. Someone remind me to remove this bit when Chocolatey 0.9.11 is released
+[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]'Tls12'
+
+Install-ChocolateyZipPackage $packageName $url $toolsDir $url64
 
 # mpv can't be shimmed, the shim doesn't work with mpv.com
 # as of 2016.01.18, there is a dll dependency, so mpv can't be hardlinked to chocolatey\bin
