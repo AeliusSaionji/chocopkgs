19547ffb044a8df9f5541ff51243e557c1f43c39
Link <link.satonaka@outlook.com>
2016-10-28 10:00:51 -0400

    x64 only

--- Hydrus-Network/tools/chocolateyinstall.ps1
+++ Hydrus-Network/tools/chocolateyinstall.ps1
@@ -3,8 +3,7 @@ $fileType = 'exe'
 $silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART'
 $messyversion = '{{PackageVersion}}'
 $version = $messyversion.Remove(3)
-$url = "https://github.com/hydrusnetwork/hydrus/releases/download/v$version/Hydrus.Network.$version.-.Windows.-.Installer.exe"
-$url64 = $url
+$url64 = "https://github.com/hydrusnetwork/hydrus/releases/download/v$version/Hydrus.Network.$version.-.Windows.-.Installer.exe"
 
 # Configure the installDir
 New-Item -Path 'HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\Hydrus Network_is1\' -ErrorAction SilentlyContinue
