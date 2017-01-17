4eaefe7c516148582ec3282305ab95dce3bf9925
Link <link.satonaka@outlook.com>
2017-01-16 21:54:30 -0500

    doublequote for vars

--- lua53/tools/chocolateyinstall.ps1
+++ lua53/tools/chocolateyinstall.ps1
@@ -1,8 +1,8 @@
 ﻿$packageName = 'lua53'
 $toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $version     = '{{PackageVersion}}'
-$url         = 'http://sourceforge.net/projects/luabinaries/files/${version}/Tools%20Executables/lua-${version}_Win32_bin.zip/download'
-$url64       = 'http://sourceforge.net/projects/luabinaries/files/${version}/Tools%20Executables/lua-${version}_Win64_bin.zip/download'
+$url         = "http://sourceforge.net/projects/luabinaries/files/${version}/Tools%20Executables/lua-${version}_Win32_bin.zip/download"
+$url64       = "http://sourceforge.net/projects/luabinaries/files/${version}/Tools%20Executables/lua-${version}_Win64_bin.zip/download"
 $checksum    = 'c3f6c0c5bde7434377e308a593fedf36587eb40786e55bd3d6378ea3a37dec9ec7fb7f5dada25e5db65d6a196a836af3de0432062614d0ea2d68d2e5b56ee183'
 $checksum64  = '4bc023f48fd79f27c32f0bc1f45daf9b00743264b096cf1ca9c579d927f148d78ca1de8cc5ca08c18995ce8858b833df3ce5abd2e3c1315dde3d837bbd118465'
 $checksumType= 'sha512'
