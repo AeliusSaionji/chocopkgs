5cceaf7f67ce0199e82cadc4e590c835748dda6d
Link <link.satonaka@outlook.com>
2017-03-09 12:51:47 -0500

    sha512, cleaning unused junk

--- GeForce-Experience/tools/chocolateyInstall.ps1
+++ GeForce-Experience/tools/chocolateyInstall.ps1
@@ -4,6 +4,8 @@ $version = '{{PackageVersion}}'
 $url = "http://us.download.nvidia.com/GFE/GFEClient/$version/GeForce_Experience_v$version.exe"
 $silentArgs = '/s /noreboot'
 $validExitCodes = @(0)
+$checksum = '{{Checksum}}'
+$checksumType = 'SHA512'
 
 Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes $validExitCodes
 Write-Output "If you experience errors after updating, you may need to reboot."
--- Xming/tools/chocolateyinstall.ps1
+++
@@ -1,7 +0,0 @@
-﻿$packageName = 'Xming'
-$fileType = 'EXE'
-$silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-' # Inno Setup
-$url = 'https://sourceforge.net/projects/xming/files/Xming/6.9.0.31/Xming-6-9-0-31-setup.exe/download'
-$validExitCodes= @(0)
-
-Install-ChocolateyPackage $packageName $fileType $silentArgs $url -validExitCodes $validExitCodes
