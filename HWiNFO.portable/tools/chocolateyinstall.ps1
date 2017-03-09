9ec4447f0921d85a8e78f7ba65e104f0d6aa1458
Link <link.satonaka@outlook.com>
2017-03-09 13:42:52 -0500

    geforce package, misc fixes

--- HWiNFO.portable/tools/chocolateyinstall.ps1
+++ HWiNFO.portable/tools/chocolateyinstall.ps1
@@ -5,8 +5,7 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url = "ftp://ftp.sac.sk/sac/utildiag/hw32_$version.zip"
 $url64 = "ftp://ftp.sac.sk/sac/utildiag/hw64_$version.zip"
 $checksum = '{{Checksum}}'
-$checksumType = 'SHA1'
+$checksumType = 'sha512'
 $checksum64 = '{{Checksumx64}}'
-$checksumType64 = 'SHA1'
 
-Install-ChocolateyZipPackage $packageName $url $toolsDir $url64 -checksum $checksum -checksumType $checksumType -checksum64 $checksum64 -checksumType64 $checksumType64
+Install-ChocolateyZipPackage $packageName $url $toolsDir $url64 -checksum $checksum -checksumType $checksumType -checksum64 $checksum64
---
+++ geforce-game-ready-driver/tools/chocolateyInstall.ps1
@@ -0,0 +1,20 @@
+$packageName = 'geforce-game-ready-driver'
+$version = '{{PackageVersion}}'
+$fileType = 'exe'
+$silentArgs = '-s -noreboot -clean'
+$checksumType = 'sha512'
+
+If ( [System.Environment]::OSVersion.Version.Major -eq '10' ) {
+	$url   = "https://us.download.nvidia.com/Windows/{{PackageVersion}}/{{PackageVersion}}-desktop-win10-32bit-international-whql.exe"
+	$url64 = "https://us.download.nvidia.com/Windows/{{PackageVersion}}/{{PackageVersion}}-desktop-win10-64bit-international-whql.exe"
+	$checksum   = '34f6edbfca3cbe70dc83589bfae339437658b2b8c291a9fc278aee0c7cabf6d495ebfb64dff8da3dc6966409ce2857a911c6a39d489c3c6b9fb5597075e04361'
+	$checksum64 = '753e7023b6ea7dd451cc1ac639d838f6f137a4153e88d6051d8217d69be631f007e90c3a672e795e31abf8a930927a7376eeca7da5852c4cf8de522f51a0fc73'
+} Else {
+	$url   = "https://us.download.nvidia.com/Windows/$version/$version-desktop-win8-win7-32bit-international-whql.exe"
+	$url64 = "https://us.download.nvidia.com/Windows/$version/$version-desktop-win8-win7-64bit-international-whql.exe"
+	$checksum   = '2f81b0778d65436708369c3283ca37fa72825eb12c512c22e92362efbf47fa0a17b4d7c86bba289b12007c5c781b33e8a9d1ae891c62c8b2a7913272ffcdd46f'
+	$checksum64 = 'd1883e733fa15ba43b76475cb9829bc8fb5edc2fb7efed49e24b3fc206b474f209f0a6b49f4e22a197c3b3c1f0e3f959b2649b394fdf0d6d4e68dafd0d4a8f00'
+}
+
+
+Install-ChocolateyPackage $packageName $fileType $silentArgs $url $url64 -Checksum $checksum -ChecksumType $checksumType -Checksum64 $checksum64
