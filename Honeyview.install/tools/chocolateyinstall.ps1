809555f10738036aa413ba5b2f36a22a7d1b5ff3
Link <link.satonaka@outlook.com>
2017-03-09 12:59:37 -0500

    sha512

--- Honeyview.install/tools/chocolateyinstall.ps1
+++ Honeyview.install/tools/chocolateyinstall.ps1
@@ -3,5 +3,9 @@ $fileType = 'EXE'
 $silentArgs = '/S'
 $url = 'http://ca-dl.bandisoft.com/honeyview/HONEYVIEW-SETUP.EXE'
 $url64 = $url
+$checksum = '{{Checksum}}'
+$checksumType = 'SHA512'
+$checksum64 = '{{Checksumx64}}'
+$checksumType64 = 'SHA512'
 
 Install-ChocolateyPackage $packageName $fileType $silentArgs $url $url64
--- Honeyview.portable/tools/chocolateyinstall.ps1
+++ Honeyview.portable/tools/chocolateyinstall.ps1
@@ -1,5 +1,7 @@
 ﻿$packageName = 'Honeyview.portable'
 $url = 'http://ca-dl.bandisoft.com/honeyview/HONEYVIEW-PORTABLE.ZIP'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$checksum = '{{Checksum}}'
+$checksumType = 'SHA512'
 
 Install-ChocolateyZipPackage $packageName $url $toolsDir
