691305f07f784fe4cc3b512f42e8b378e0f19a86
Link <link.satonaka@outlook.com>
2017-04-08 03:50:37 +0000

    stop process started by installer

--- Honeyview.install/tools/chocolateyinstall.ps1
+++ Honeyview.install/tools/chocolateyinstall.ps1
@@ -9,3 +9,6 @@ $checksumType = 'SHA512'
 
 Install-ChocolateyPackage $packageName $fileType $silentArgs $url $url64 `
 -Checksum $checksum -ChecksumType $checksumType -Checksum64 $checksum64
+
+# Kill the instance autostarted by the installer
+Get-Process | Where {$_.name -eq 'Honeyview'} | Stop-Process
