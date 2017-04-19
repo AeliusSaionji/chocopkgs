1dc523c26ff344bd704f3e335fa326e8373b95a1
Link <link.satonaka@outlook.com>
2017-04-19 00:25:33 +0000

    echo off, be less dumb about error handling

--- wsltty/tools/create-dirs.bat
+++ wsltty/tools/create-dirs.bat
@@ -1,8 +1,9 @@
+@ECHO OFF
 REM This section was inserted by the chocolatey package
 REM Create config dirs
-mkdir "%LOCALAPPDATA%\wsltty"
-mkdir "%APPDATA%\mintty"
-if not exist "%LOCALAPPDATA%\wsltty\config" copy NUL "%LOCALAPPDATA%\wsltty\config"
-mklink /J "%APPDATA%\mintty\lang" "%ChocolateyInstall%\lib\wsltty\tools\lang"
+IF NOT EXIST "%LOCALAPPDATA%\wsltty" MKDIR "%LOCALAPPDATA%\wsltty"
+IF NOT EXIST "%LOCALAPPDATA%\wsltty\config" COPY NUL "%LOCALAPPDATA%\wsltty\config"
+IF NOT EXIST "%APPDATA%\mintty" MKDIR "%APPDATA%\mintty"
+IF NOT EXIST "%APPDATA%\mintty\lang" MKLINK /J "%APPDATA%\mintty\lang" "%ChocolateyInstall%\lib\wsltty\tools\lang"
 REM This section was inserted by the chocolatey package
 
