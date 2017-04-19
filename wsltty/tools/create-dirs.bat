39dc393d10c894438ee372df4524c64083a8ea95
Link <link.satonaka@outlook.com>
2017-04-19 00:06:43 +0000

    don't overwite config

--- wsltty/tools/create-dirs.bat
+++ wsltty/tools/create-dirs.bat
@@ -1,7 +1,8 @@
-REM This top section was inserted by the chocolatey package
+REM This section was inserted by the chocolatey package
 REM Create config dirs
 mkdir "%LOCALAPPDATA%\wsltty"
-copy NUL "%LOCALAPPDATA%\wsltty\config"
 mkdir "%APPDATA%\mintty"
+if not exist "%LOCALAPPDATA%\wsltty\config" copy NUL "%LOCALAPPDATA%\wsltty\config"
 mklink /J "%APPDATA%\mintty\lang" "%ChocolateyInstall%\lib\wsltty\tools\lang"
+REM This section was inserted by the chocolatey package
 
