a60eeeacd95557e7a38579cebbc7506e8f05f7ce
Link <link.satonaka@outlook.com>
2017-05-06 14:18:42 -0400

    renaming

--- LatencyMon/tools/chocolateyinstall.ps1
+++
@@ -1,9 +0,0 @@
-﻿$packageName = 'LatencyMon'
-$fileType = 'EXE'
-$silentArgs = '/SP- /VERYSILENT /SUPPRESSMSGBOXES /NORESTART'
-$url = 'http://www.resplendence.com/download/LatencyMon.exe'
-$checksum = '{{Checksum}}'
-$checksumType = 'SHA512'
-
-Install-ChocolateyPackage -PackageName $packageName -FileType $fileType -SilentArgs $silentArgs -Url $url `
--Checksum $checksum -ChecksumType $checksumType
