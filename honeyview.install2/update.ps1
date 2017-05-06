3f4a0751ffaa66efa03ce84e8ea90d4a8a83dd68
Link <link.satonaka@outlook.com>
2017-05-06 13:53:41 -0400

    some fixes and renaming

---
+++ honeyview.install2/update.ps1
@@ -0,0 +1,33 @@
+Import-Module au
+
+$releases = "http://www.bandisoft.com/honeyview/"
+
+function global:au_SearchReplace {
+        @{
+                ".\legal\VERIFICATION.txt" = @{
+                "(?i)(^\s*checksum\s*type\:).*"     = "`${1} $($Latest.ChecksumType32)"
+                "(?i)(^\s*checksum(32)?\:).*"       = "`${1} $($Latest.Checksum32)"
+                }
+        }
+}
+
+if ($MyInvocation.InvocationName -ne '.') { # run the update only if script is not sourced
+	function au_BeforeUpdate() {
+	    #Download $Latest.URL32 / $Latest.URL64 in tools directory and remove any older installers.
+	    Get-RemoteFiles -Purge
+	}
+}
+
+function global:au_GetLatest {
+        $url = 'https://dl.bandisoft.com/honeyview/HONEYVIEW-SETUP.EXE'
+	$download_page = (iwr $releases -UseBasicParsing).Content.Split("`n") | Select-String 'Honeyview v'
+	$Matches = $null
+	$download_page -match "\d+\.\d+"
+	$version = $Matches[0]
+
+	return @{ Version = $version; URL32 = $url }
+}
+
+if ($MyInvocation.InvocationName -ne '.') { # run the update only if script is not sourced
+	Update-Package -ChecksumFor none
+}
---
+++ honeyview.portable2/tools/chocolateyinstall.ps1
@@ -0,0 +1,22 @@
+﻿$ErrorActionPreference = 'Stop'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$filePath = "$toolsDir\HONEYVIEW-PORTABLE_x32.ZIP"
+
+# Place shortcuts in appropriate location
+$ProgsFolder = [environment]::getfolderpath('Programs')
+If ( Test-ProcessAdminRights ) {
+  $ProgsFolder = Join-Path ([environment]::getfolderpath('CommonApplicationData')) "Microsoft\Windows\Start Menu\Programs"
+}
+
+$packageArgs = @{
+	packageName      = 'Honeyview.portable'
+	file             = "$filePath"
+	destination      = "$toolsDir"
+        shortcutFilePath = "$ProgsFolder\Honeyview.lnk"
+        targetPath       = "$toolsDir\Honeyview32.exe"
+}
+
+Get-ChocolateyUnzip @packageArgs
+Install-ChocolateyShortcut @packageArgs
+
+Remove-Item -Force -ea 0 $filePath
