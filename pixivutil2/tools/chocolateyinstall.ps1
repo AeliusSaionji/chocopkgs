24532ccca8c6f0a50014c92ee33d4117726a7066
Link <link.satonaka@outlook.com>
2017-08-07 23:57:37 -0400

    update.ps1 created

--- pixivutil2/tools/chocolateyinstall.ps1
+++ pixivutil2/tools/chocolateyinstall.ps1
@@ -1,10 +1,8 @@
 ﻿$packageName = 'PixivUtil2'
-$version = '{{PackageVersion}}'
-$version = $version.Replace('.','')
 $unzipLocation = Join-Path "$(Get-BinRoot)" "$packageName"
-$url = "https://github.com/Nandaka/PixivUtil2/releases/download/v$version/pixivutil$version.7z"
-$checksum = '{{Checksum}}'
-$checksumType = 'SHA512'
+$url = 'https://github.com/Nandaka/PixivUtil2/releases/download/v20170705/pixivutil20170705.7z'
+$checksum = 'a313045f2d978aa335a2708f9c469d2d4e415fbe0fb7b01c9a11a3ea8ab1e7cd'
+$checksumType = 'sha256'
 
 Install-ChocolateyZipPackage $packageName $url $unzipLocation `
 -Checksum $checksum -ChecksumType $checksumType
---
+++ pixivutil2/update.ps1
@@ -0,0 +1,33 @@
+﻿Import-Module au
+
+$releases = "https://github.com/Nandaka/PixivUtil2/releases/latest"
+
+function global:au_SearchReplace {
+        @{
+                ".\tools\chocolateyinstall.ps1" = @{
+                "(?i)(^\s*[$]checksumType\s*=\s*)('.*')" = "`${1}'$($Latest.ChecksumType32)'"
+                "(?i)(^\s*[$]checksum\s*=\s*)('.*')"     = "`${1}'$($Latest.Checksum32)'"
+                "(?i)(^\s*[$]url\s*=\s*)('.*')"          = "`${1}'$($Latest.URL32)'"
+                }
+        }
+}
+
+
+
+function global:au_BeforeUpdate {
+	$Latest.ChecksumType32 = 'sha256'
+	$Latest.Checksum32 = Get-RemoteChecksum -Url $Latest.URL32 -Algorithm $Latest.ChecksumType32
+}
+
+function global:au_GetLatest {
+	$download_page = (iwr $releases -UseBasicParsing).Links.href | Select-String '/tag/v' | Select-Object -First 1
+	$Matches = $null
+	$download_page -match '\d\d\d\d\d\d\d\d'
+	$urlvers = $Matches[0]
+	$version = [datetime]::ParseExact($Matches[0],'yyyyMMdd',$null) | Get-Date -Format yyyy.MM.dd
+	$url32 = "https://github.com/Nandaka/PixivUtil2/releases/download/v$urlvers/pixivutil$urlvers.7z"
+
+	return @{ Version = $version; URL32 = $url32 }
+}
+
+Update-Package -ChecksumFor none
