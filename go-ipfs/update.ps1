73f58882dcc31de33aaa915f027f2ad1e2df4c05
Aelius <aeliussaionji@outlook.com>
2017-11-19 21:39:30 -0500

    remove x86 search

--- go-ipfs/update.ps1
+++ go-ipfs/update.ps1
@@ -4,17 +4,21 @@ $releases = "https://github.com/ipfs/go-ipfs/releases"
 
 function global:au_SearchReplace {
         @{
-                ".\tools\chocolateyinstall.ps1" = @{
-                "(?i)(^\s*[$]checksumType\s*=\s*)('.*')" = "`${1}'$($Latest.ChecksumType32)'"
-                "(?i)(^\s*[$]checksum64\s*=\s*)('.*')"   = "`${1}'$($Latest.Checksum64)'"
-                "(?i)(^\s*[$]checksum\s*=\s*)('.*')"     = "`${1}'$($Latest.Checksum32)'"
-                "(?i)(^\s*[$]url64\s*=\s*)('.*')"        = "`${1}'$($Latest.URL64)'"
-                "(?i)(^\s*[$]url\s*=\s*)('.*')"          = "`${1}'$($Latest.URL32)'"
-                }
-                ".\go-ipfs.nuspec" = @{
-		"(?i)(^\s*<releaseNotes>)(.*)" = "`${1}https://github.com/ipfs/go-ipfs/blob/v$($Latest.Version)/CHANGELOG.md</releaseNotes>"
-		}
+				".\tools\chocolateyinstall.ps1" = @{
+				"(?i)(^\s*[$]checksumType64\s*=\s*)('.*')" = "`${1}'$($Latest.ChecksumType64)'"
+				"(?i)(^\s*[$]checksum64\s*=\s*)('.*')"     = "`${1}'$($Latest.Checksum64)'"
+				"(?i)(^\s*[$]url64\s*=\s*)('.*')"          = "`${1}'$($Latest.URL64)'"
+				}
+				".\go-ipfs.nuspec" = @{
+					"(?i)(^\s*<releaseNotes>)(.*)" = "`${1}https://github.com/ipfs/go-ipfs/blob/v$($Latest.Version)/CHANGELOG.md</releaseNotes>"
+				}
 	}
+}
+
+
+function global:au_BeforeUpdate {
+	$Latest.ChecksumType64 = 'sha256'
+	$Latest.Checksum64 = Get-RemoteChecksum -Url $Latest.URL64 -Algorithm $Latest.ChecksumType64
 }
 
 function global:au_GetLatest {
@@ -22,11 +26,10 @@ function global:au_GetLatest {
 	$Matches = $null
 	$download_page -match '\d+\.\d+\.\d+'
 	$version = $Matches[0]
-	# TODO - check for RC release and append -beta to version?
-	$url   = "https://dist.ipfs.io/go-ipfs/v$version/go-ipfs_v${version}_windows-386.zip"
+	#$url   = "https://dist.ipfs.io/go-ipfs/v$version/go-ipfs_v${version}_windows-386.zip"
 	$url64 = "https://dist.ipfs.io/go-ipfs/v$version/go-ipfs_v${version}_windows-amd64.zip"
 
-	return @{ Version = $version; URL32 = $url; URL64 = $url64 }
+	return @{ Version = $version; URL64 = $url64 }
 }
 
-Update-Package
+Update-Package -ChecksumFor none
