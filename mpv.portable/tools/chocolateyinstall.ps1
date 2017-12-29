065e06db0443693c13d0916e1de7aa41104ee9c5
Bennett Blodinger <benwa@users.noreply.github.com>
2017-12-29 10:19:03 -0500

    mpv 2017-12-25

--- mpv.portable/tools/chocolateyinstall.ps1
+++ mpv.portable/tools/chocolateyinstall.ps1
@@ -1,15 +1,12 @@
 ﻿$packageName = 'mpv.portable'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$version = '2017.09.13'
+$version = '2017.12.25'
 $version = $version.Replace(".","")
 $url = "https://mpv.srsfckn.biz/mpv-i686-$version.7z"
 $url64 = "https://mpv.srsfckn.biz/mpv-x86_64-$version.7z"
-$checksum = '980d5e000972e1108486ea605ebc5498070a9c75c22d9d83b2caca4fd7435e64c2bb4b61ef4c802ea662491f3bd2ff9a8bcec6c1ea75947d436aa2f823002dbc'
-$checksum64 = '2002b41827ff3aab4ebb38d0ad3442312405085473e3078babbbecb987775e6d5fef9538092c659288202dc43562813d6d971e4063ad9a40a94787920ee0e4c3'
+$checksum = '122AE8D1CE7E7F07AA594CA9F23B58A65B6D0574910692F2D8CB6811A4B0964838156F4E4858F896B49E8C563DA8DCEE06FF15EA0E4DEBF29149D83A2FE3D467'
+$checksum64 = '2DF4D375C9F67F7A9EFD638AE04515060305930FFA07596985F9BF457A81936578C0FB4E7B3426DB45C8679D7B09651177CC4AC5DBAE25AA15E8FF2A3E07D9F3'
 $checksumType = 'SHA512'
-
-# Enable TLS 1.2 manually. Someone remind me to remove this bit when Chocolatey 0.9.11 is released
-[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]'Tls12'
 
 Install-ChocolateyZipPackage $packageName $url $toolsDir $url64 `
 -Checksum $checksum -ChecksumType $checksumType -Checksum64 $checksum64
