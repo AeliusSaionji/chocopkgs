fbdfee18d977d4f195b8ecec68e39e02259c4c23
Aelius <aeliussaionji@outlook.com>
2018-03-27 15:46:27 -0400

    more formatting, fix update.ps1

--- wsltty/update.ps1
+++ wsltty/update.ps1
@@ -1,4 +1,4 @@
-﻿Import-Module au
+Import-Module au
 
 # At the time of running, github is only operating with 1.2
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
@@ -7,7 +7,7 @@ $releases = "https://github.com/mintty/wsltty/releases/latest"
 function global:au_SearchReplace {
 	@{
 		".\legal\VERIFICATION.txt" = @{
-        "(?i)(\s+x32:).*"                   = "`${1} $($Latest.URL32)"
+		"(?i)(\s+x32:).*"                   = "`${1} $($Latest.URL32)"
 		"(?i)(^\s*checksum\s*type\:).*"     = "`${1} $($Latest.ChecksumType32)"
 		"(?i)(^\s*checksum32\:).*"          = "`${1} $($Latest.Checksum32)"
 		}
@@ -25,5 +25,5 @@ function global:au_GetLatest {
 }
 
 if ($MyInvocation.InvocationName -ne '.') { # run the update only if script is not sourced
-	Update-Package
+	Update-Package -NoCheckURL -ChecksumFor none
 }
