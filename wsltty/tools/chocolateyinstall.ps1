f879ce9296092c9229cf91c124feb2e1f77d93c7
Aelius <aeliussaionji@outlook.com>
2018-03-27 15:29:29 -0400

    wsltty finally working

--- wsltty/tools/chocolateyinstall.ps1
+++ wsltty/tools/chocolateyinstall.ps1
@@ -2,11 +2,14 @@ $packageName = 'wsltty'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $filePath = Get-Item "$toolsdir\*install.exe"
 
-# Extract installer to temp
-Start-Process -FilePath "$filePath" -ArgumentList "/T:$ENV:TEMP\wslttyinstall /C /Q" -WorkingDirectory "$toolsdir" -Wait
-# Run install.bat
-Start-Process -FilePath "$ENV:TEMP\wslttyinstall\install.bat" -ArgumentList "$toolsDir" -WorkingDirectory "$ENV:TEMP\wslttyinstall" -Wait
+# Extract installer
+Start-Process -FilePath "$filePath" -ArgumentList "/T:$toolsDir\wslttyinstall /C /Q" -WorkingDirectory "$toolsdir" -Wait
+# Create shortcut
+Install-ChocolateyShortcut `
+ -ShortcutFilePath "$ENV:ALLUSERSPROFILE\Microsoft\Windows\Start Menu\Programs\WSL Generate Shortcuts.lnk" `
+ -TargetPath "$toolsDir\wslttyinstall\install.bat" `
+ -WorkingDirectory "$toolsDir\wslttyinstall"
 # Remove installer
 Remove-Item -Force -ea 0 "$filePath"
 # Generate *.exe.ignore
-gi "$toolsDir\bin\*.exe" | % { New-Item "$($_.FullName).ignore" -type File }
+gi "$toolsDir\wslttyinstall\*.exe" | % { New-Item "$($_.FullName).ignore" -type File }
--- wsltty/tools/chocolateyuninstall.ps1
+++ wsltty/tools/chocolateyuninstall.ps1
@@ -1,6 +1,1 @@
-$packageName = 'wsltty'
-$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-
-Uninstall-BinFile -Name 'wsl'
-Uninstall-BinFile -Name 'wsl~'
-Uninstall-BinFile -Name 'wsl-l'
+Remove-Item -Force -ea 0 "$ENV:ALLUSERSPROFILE\Microsoft\Windows\Start Menu\Programs\WSL Generate Shortcuts.lnk"
