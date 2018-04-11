5fa427583d1ad85011a6cc363b20ab7a7f2cf64e
Aelius <aeliussaionji@outlook.com>
2018-04-11 00:57:11 -0400

    bump

--- nvidia-display-driver/tools/chocolateyInstall.ps1
+++ nvidia-display-driver/tools/chocolateyInstall.ps1
@@ -5,17 +5,17 @@ $unpackFile = "${ENV:TEMP}\nvidiadriver.zip"
 $unpackDir  = "${ENV:TEMP}\nvidiadriver"
 $instDir    = "${ENV:TEMP}\nvidiainstall"
 $file = "$instDir\setup.exe"
-$url   = 'https://us.download.nvidia.com/Windows/391.24/391.24-desktop-win10-32bit-international-whql.exe'
-$url64 = 'https://us.download.nvidia.com/Windows/391.24/391.24-desktop-win10-64bit-international-whql.exe'
-$checksum   = '5d97cf8f4910c67b62de71f546cff34d8344557b9144f6262c85ee4ba1e6242e'
-$checksum64 = '6c03c8deb7724b44f71333f06e8799d033a0d658e6d9d0a8841d72a6d4d08640'
+$url   = 'https://us.download.nvidia.com/Windows/391.35/391.35-desktop-win10-32bit-international-whql.exe'
+$url64 = 'https://us.download.nvidia.com/Windows/391.35/391.35-desktop-win10-64bit-international-whql.exe'
+$checksum   = '5128180228d63d550869d615d9d165eabbc1bc1ce1c5a8235466e16d4867cb4e'
+$checksum64 = 'd352ad886b250482385ae1b597cf1be301e2e3378567e10feb8942fec0c964c2'
 $checksumType = 'sha256'
 
 If ( [System.Environment]::OSVersion.Version.Major -ne '10' ) {
-	$url   = 'https://us.download.nvidia.com/Windows/391.24/391.24-desktop-win8-win7-32bit-international-whql.exe'
-	$url64 = 'https://us.download.nvidia.com/Windows/391.24/391.24-desktop-win8-win7-64bit-international-whql.exe'
-	$checksum   = 'eab6a65bc57dbdd63b08ec9f601121c92caed70686d7a80a411772f3b4673f51'
-	$checksum64 = '50a8b696d7374d11005639daaff189f5f08de66d685b41ad1e3d7342144594ca'
+	$url   = 'https://us.download.nvidia.com/Windows/391.35/391.35-desktop-win8-win7-32bit-international-whql.exe'
+	$url64 = 'https://us.download.nvidia.com/Windows/391.35/391.35-desktop-win8-win7-64bit-international-whql.exe'
+	$checksum   = '1687319094fbcf7bb3e45ffa713916d94ebd9c838672720558553132becd2ac3'
+	$checksum64 = '7dd05b645433129b86ced17fcd8d29bf98eca2f3ac4834c19f24fb1161ea9e5c'
 }
 
 # Clean up
