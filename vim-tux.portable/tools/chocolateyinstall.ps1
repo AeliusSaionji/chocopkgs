8b54a492bf3944f9675fc6b4e672a9be60f28045
Aelius <aeliussaionji@outlook.com>
2018-04-13 22:12:18 -0400

    fix vim-tux.portable. usable!

--- vim-tux.portable/tools/chocolateyinstall.ps1
+++ vim-tux.portable/tools/chocolateyinstall.ps1
@@ -2,20 +2,21 @@
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $destDir = Join-Path $toolsDir "vim80"
 if ($Env:ChocolateyPackageParameters -match '/InstallDir:\s*(.+)') {
-    $destDir = $Matches[1]
-    if ($destDir.StartsWith("'") -or $destDir.StartsWith('"')) { $destDir = $destDir -replace '^.|.$' }
+	$destDir = $Matches[1]
+	if ($destDir.StartsWith("'") -or $destDir.StartsWith('"')) { $destDir = $destDir -replace '^.|.$' }
 }
-$filePath = if ((Get-ProcessorBits 64) -and $env:chocolateyForceX86 -ne $true) {
-       Write-Host "Installing 64 bit version" ; gi $toolsDir\*_x64.7z }
-else { Write-Host "Installing 32 bit version" ; gi $toolsDir\*_x32.7z }
 
 $packageArgs = @{
-        packageName      = 'vim-tux.portable'
-        file             = "$filePath"
-        destination      = "$destDir"
+	packageName = 'vim-tux.portable'
+	filetype    = 'exe'
+	file        = gi $toolsDir\*_x32.exe
+	file64      = gi $toolsDir\*_x64.exe
+	silentArgs  = "-o`"$destDir`" -y"
 }
 
-Get-ChocolateyUnzip @packageArgs
+Install-ChocolateyPackage @packageArgs
+# Rather than letting the installer place the batch files in C:\WINDOWS, I have included them in this package.
+# Installing them in this way ensures the package is compatible with non-admin installs, as a good portable package should :)
 Get-ChildItem "$destDir\*.bat" | %{ Install-BinFile -Name $_.BaseName -Path $_ }
-Start-Process "$destDir\install.exe" -ArgumentList "-add-start-menu" -WorkingDirectory "$destDir"
+Remove-Item -Force -ea 0 "$toolsDir\*_x32.exe","$toolsDir\*_x64.exe"
 Write-Output "Build provided by TuxProject.de - consider donating to help support their server costs."
--- vim-tux.portable/tools/chocolateyuninstall.ps1
+++ vim-tux.portable/tools/chocolateyuninstall.ps1
@@ -3,4 +3,3 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $destDir = Join-Path $toolsDir "vim80"
 
 Get-ChildItem "$destDir\*.bat" | %{ Uninstall-BinFile -Name $_.BaseName -Path $_ }
-Start-Process "$destDir\uninstal.exe" -ArgumentList "-nsis" -Wait
