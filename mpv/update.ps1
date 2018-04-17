54263c01bd38e60819d14d1d564a4bdef06917e5
Aelius <aeliussaionji@outlook.com>
2018-04-17 03:57:55 -0400

    mpv tweaks

---
+++ mpv/update.ps1
@@ -0,0 +1,16 @@
+Import-Module au
+
+. $PSScriptRoot\..\mpv.install\update.ps1
+
+function global:au_SearchReplace {
+	@{
+		"$($Latest.PackageName).nuspec" = @{
+			"(\<dependency .+?`"$($Latest.PackageName).install`" version=)`"([^`"]+)`"" = "`$1`"$($Latest.Version)`""
+		}
+	}
+}
+
+# Left empty intentionally
+function global:au_BeforeUpdate { }
+
+Update-Package -ChecksumFor none
