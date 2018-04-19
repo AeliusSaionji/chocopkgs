b12d9dbd699e3a43c79a5ce9032c21f93e59bb1f
Aelius <aeliussaionji@outlook.com>
2018-04-19 01:21:25 -0400

    embed + update.ps1

--- x264vfw/tools/chocolateyinstall.ps1
+++ x264vfw/tools/chocolateyinstall.ps1
@@ -1,10 +1,8 @@
-﻿$packageName = 'x264vfw'
-$fileFullPath = "$ENV:Temp\x264vfw.exe"
-$fileType = 'EXE'
-$silentArgs = '/S'
-$url = 'http://sourceforge.net/projects/x264vfw/files/latest/download'
-$checksum = '{{Checksum}}'
-$checksumType = 'SHA512'
+﻿$packageArgs = @{
+    packageName = 'x264vfw'
+    filetype    = 'EXE'
+    file        = gi $toolsDir\*_x32.exe
+    silentArgs  = "/S"
+}
 
-Install-ChocolateyPackage $packageName $fileType $silentArgs $url `
--Checksum $checksum -ChecksumType $checksumType
+Install-ChocolateyPackage @packageArgs
---
+++ x264vfw/update.ps1
@@ -0,0 +1,30 @@
+import-module au
+
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+$releases = 'https://sourceforge.net/projects/x264vfw/files/x264vfw/'
+
+function global:au_SearchReplace {
+	@{
+	".\legal\VERIFICATION.txt" = @{
+		"(?i)(\s+x32:).*"            = "`${1} $($Latest.URL32)"
+		"(?i)(Get-RemoteChecksum).*" = "`${1} $($Latest.URL32)"
+		"(?i)(checksum32:).*"        = "`${1} $($Latest.Checksum32)"
+		}
+	}
+}
+
+function global:au_BeforeUpdate {
+	Get-RemoteFiles -Purge
+}
+
+function global:au_GetLatest {
+	$download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+	$url = $download_page.links | ? href -match 'x264vfw/[0-9]+' | % href | select -First 1 | % { 'https://sourceforge.net' + $_ }
+	$download_files_page = Invoke-WebRequest -Uri $url -UseBasicParsing
+	$url32 = $download_files_page.Links | ? {$_ -match '.exe/download'} | select -first 1 -expand href | % { $_.TrimEnd('/download')}
+	$download_files_page.Content -split '\n' | sls 'released on' | ? {$_ -match '[\d]+-[\d]+-[\d]+'; $dashVersion = $Matches[0]}
+	$version = $dashVersion -replace ('-','.')
+	@{ URL32 = $url32; Version = $version }
+}
+
+Update-Package -ChecksumFor none
