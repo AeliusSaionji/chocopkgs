f21ca5743d650ce8a81439e12b30474574614544
Aelius <aeliussaionji@outlook.com>
2018-04-19 02:30:42 -0400

    initial pulseaudio commit

---
+++ pulseaudio/tools/chocolateyinstall.ps1
@@ -0,0 +1,13 @@
+﻿$ErrorActionPreference = 'Stop'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$filePath = "$toolsDir\pulseaudio-1.1.zip"
+
+$packageArgs = @{
+	packageName      = 'pulseaudio'
+	file             = "$filePath"
+	destination      = "$toolsDir"
+}
+
+Get-ChocolateyUnzip @packageArgs
+
+Remove-Item -Force -ea 0 $filePath
