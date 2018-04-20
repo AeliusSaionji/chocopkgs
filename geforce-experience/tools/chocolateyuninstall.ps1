93572f95252d307d37a6b3839330f0f5e97da1f5
Aelius <aeliussaionji@outlook.com>
2018-04-20 02:11:20 -0400

    gfexperience uninstall script!

---
+++ geforce-experience/tools/chocolateyuninstall.ps1
@@ -0,0 +1,12 @@
+﻿$ErrorActionPreference = 'Stop';
+$packageArgs = @{
+  packageName   = 'geforce-experience'
+  softwareName  = 'NVIDIA Geforce Experience*'
+  fileType      = 'EXE'
+  validExitCodes= @(0)
+}
+
+$packageArgs['file']       = (Get-UninstallRegistryKey 'NVIDIA Geforce Experience*').UninstallString -replace "\s.*$"
+$packageArgs['silentArgs'] = (Get-UninstallRegistryKey 'NVIDIA Geforce Experience*').UninstallString -replace "^.*?\s" -replace "$"," -silent"
+
+Uninstall-ChocolateyPackage @packageArgs
