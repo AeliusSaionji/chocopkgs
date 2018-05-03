f1f6b40284c44745aec1e3056a8eafdc28ac1f9c
Aelius <aeliussaionji@outlook.com>
2018-05-03 10:20:21 -0400

    embed ipfs

--- go-ipfs/tools/chocolateyinstall.ps1
+++ go-ipfs/tools/chocolateyinstall.ps1
@@ -1,10 +1,12 @@
-﻿$packageName= 'go-ipfs'
-$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-$url64      = 'https://dist.ipfs.io/go-ipfs/v0.4.14/go-ipfs_v0.4.14_windows-amd64.zip'
+$packageArgs = @{
+    packageName = 'go-ipfs'
+    file        = gi "$toolsDir\*x32.zip"
+    file64      = gi "$toolsDir\*x64.zip"
+    destination = $toolsDir
+}
 
-$checksum64 = '38a0abd9a610670d4a3315384bfb7e77dbdac010da6b6394cc5ac96762e0f67d'
-$checksumType64 = 'sha256'
-
-Install-ChocolateyZipPackage $packageName -UnzipLocation $toolsDir -Url64bit $url64 `
--ChecksumType64 $checksumType64 -Checksum64 $checksum64
+Get-ChocolateyUnzip @packageArgs
+Remove-Item -force "$toolsDir\*.zip" -ea 0
--- go-ipfs/update.ps1
+++ go-ipfs/update.ps1
@@ -1,35 +1,33 @@
 Import-Module au
 
-$releases = "https://github.com/ipfs/go-ipfs/releases"
+$releases = "https://dist.ipfs.io/#go-ipfs"
 
 function global:au_SearchReplace {
-        @{
-				".\tools\chocolateyinstall.ps1" = @{
-				"(?i)(^\s*[$]checksumType64\s*=\s*)('.*')" = "`${1}'$($Latest.ChecksumType64)'"
-				"(?i)(^\s*[$]checksum64\s*=\s*)('.*')"     = "`${1}'$($Latest.Checksum64)'"
-				"(?i)(^\s*[$]url64\s*=\s*)('.*')"          = "`${1}'$($Latest.URL64)'"
-				}
-				".\go-ipfs.nuspec" = @{
-					"(?i)(^\s*<releaseNotes>)(.*)" = "`${1}https://github.com/ipfs/go-ipfs/blob/v$($Latest.Version)/CHANGELOG.md</releaseNotes>"
-				}
+	@{
+		".\legal\VERIFICATION.txt" = @{
+			"(?i)(\s+x32:).*"               = "`${1} $($Latest.URL32)"
+			"(?i)(\s+x64:).*"               = "`${1} $($Latest.URL64)"
+			"(?i)(^\s*checksum\s*type\:).*" = "`${1} $($Latest.ChecksumType32)"
+			"(?i)(^\s*checksum32\:).*"      = "`${1} $($Latest.Checksum32)"
+			"(?i)(^\s*checksum64\:).*"      = "`${1} $($Latest.Checksum64)"
+		}
+		".\go-ipfs.nuspec" = @{
+			"(?i)(^\s*<releaseNotes>)(.*)" = "`${1}https://github.com/ipfs/go-ipfs/blob/v$($Latest.Version)/CHANGELOG.md</releaseNotes>"
+		}
 	}
 }
 
-
-function global:au_BeforeUpdate {
-	$Latest.ChecksumType64 = 'sha256'
-	$Latest.Checksum64 = Get-RemoteChecksum -Url $Latest.URL64 -Algorithm $Latest.ChecksumType64
-}
+function global:au_BeforeUpdate { Get-RemoteFiles -Purge }
 
 function global:au_GetLatest {
-	$download_page = (iwr $releases -UseBasicParsing).Links.href | Select-String '/tag/v' | Select-Object -First 1
+	$download_page = (iwr $releases -UseBasicParsing).Content -split '\n' | sls 'data-id="go-ipfs" data-version="'
 	$Matches = $null
 	$download_page -match '\d+\.\d+\.\d+'
 	$version = $Matches[0]
-	#$url   = "https://dist.ipfs.io/go-ipfs/v$version/go-ipfs_v${version}_windows-386.zip"
+	$url32 = "https://dist.ipfs.io/go-ipfs/v$version/go-ipfs_v${version}_windows-386.zip"
 	$url64 = "https://dist.ipfs.io/go-ipfs/v$version/go-ipfs_v${version}_windows-amd64.zip"
 
-	return @{ Version = $version; URL64 = $url64 }
+	return @{ Version = $version; URL32 = $url32; URL64 = $url64 }
 }
 
 Update-Package -ChecksumFor none
