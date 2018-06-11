cb491501b113ca572cda64d3acd4d09a99ef2dd7
Chocolatey <chocolatey@realdimensions.net>
2018-06-11 00:10:51 +0000

    AU: 2 updated - qutebrowser qutebrowser.portable [skip ci]

--- qutebrowser.portable/tools/chocolateyinstall.ps1
+++ qutebrowser.portable/tools/chocolateyinstall.ps1
@@ -1,9 +1,9 @@
 ﻿$packageName = 'qutebrowser.portable'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url = 'https://github.com/qutebrowser/qutebrowser/releases/download/v1.3.0/qutebrowser-1.3.0-windows-standalone-win32.zip'
-$url64 = 'https://github.com/qutebrowser/qutebrowser/releases/download/v1.3.0/qutebrowser-1.3.0-windows-standalone-amd64.zip'
-$checksum = 'cf6fac19622be05fd42109e18765d38ffb22419ef9cf42b85e2dfc352cc47899'
-$checksum64 = 'ca52f2d33c7388dd60990f6339b0cbbf2a6d1bc80a401d9a48e6cb5f9d97b47e'
+$url = 'https://github.com/qutebrowser/qutebrowser/releases/download/v1.3.2/qutebrowser-1.3.2-windows-standalone-win32.zip'
+$url64 = 'https://github.com/qutebrowser/qutebrowser/releases/download/v1.3.2/qutebrowser-1.3.2-windows-standalone-amd64.zip'
+$checksum = '4a4620320b21894ab609e2cab62a54cf2dff0e55292c169943549e80a39d3878'
+$checksum64 = '80fe5bf293e96c9b1778e8a00f1e04ad712eac3b09f99c173607b78b2aa38578'
 $checksumType = 'sha256'
 
 Install-ChocolateyZipPackage $packageName $url $toolsDir $url64 `
