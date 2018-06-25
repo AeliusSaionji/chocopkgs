51a7fd88f7e85e5d15b29fb1b39d674a841a91b0
Aelius <4342746+AeliusSaionji@users.noreply.github.com>
2018-06-25 16:23:24 -0400

    updater for latencymon

--- latencymon/tools/chocolateyinstall.ps1
+++ latencymon/tools/chocolateyinstall.ps1
@@ -1,9 +1,10 @@
-﻿$packageName = 'LatencyMon'
-$fileType = 'EXE'
-$silentArgs = '/SP- /VERYSILENT /SUPPRESSMSGBOXES /NORESTART'
-$url = 'http://www.resplendence.com/download/LatencyMon.exe'
-$checksum = 'ad014cb1b2fb7da4c9b55683309cebb043108ae96c87589a46a29c55b27cd01a'
-$checksumType = 'SHA256'
+﻿$packageArgs = @{
+  PackageName = 'LatencyMon'
+  fileType = 'EXE'
+  SilentArgs = '/SP- /VERYSILENT /SUPPRESSMSGBOXES /NORESTART'
+  url = 'http://www.resplendence.com/download/LatencyMon.exe'
+  checksum = 'd7bd679030c2c2986c4c88e3935a53fe0acb850e568db274e39c60014b5b6ae1'
+  checksumType = 'SHA256'
+}
 
-Install-ChocolateyPackage -PackageName $packageName -FileType $fileType -SilentArgs $silentArgs -Url $url `
--Checksum $checksum -ChecksumType $checksumType
+Install-ChocolateyPackage @packageArgs
---
+++ latencymon/update.ps1
@@ -0,0 +1,27 @@
+Import-Module au
+
+$releases = "http://www.resplendence.com/latencymon"
+
+function global:au_SearchReplace {
+	@{
+		".\tools\chocolateyinstall.ps1" = @{
+			"(?i)(^\s*checksum\s=).*"      = "`${1} `'$($Latest.Checksum32)`'"
+			"(?i)(^\s*checksumtype\s=).*"  = "`${1} `'$($Latest.ChecksumType32)`'"
+		}
+	}
+}
+function global:au_BeforeUpdate {
+	$Latest.ChecksumType32 = 'SHA256'
+	$Latest.CheckSum32 = Get-RemoteChecksum -Url 'http://www.resplendence.com/download/LatencyMon.exe' -Algorithm $Latest.ChecksumType32
+}
+
+function global:au_GetLatest {
+	$download_page = (iwr $releases -UseBasicParsing).Content -split '\n' | sls 'Latest version: LatencyMon v'
+	$Matches = $null
+	$download_page -match '\d+\.\d+'
+	$version = $Matches[0]
+
+	return @{ Version = $version }
+}
+
+Update-Package -checksumFor none
