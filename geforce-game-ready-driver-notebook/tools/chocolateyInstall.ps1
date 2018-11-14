3dc6ae302ab73f6236df8f760c64c01763f282ee
OttNorml <ottnorml@gmail.com>
2018-11-14 14:49:03 +0100

    Copy original geforce-game-ready-driver package files

---
+++ geforce-game-ready-driver-notebook/tools/chocolateyInstall.ps1
@@ -0,0 +1,27 @@
+﻿$ErrorActionPreference = 'Stop';
+$packageArgs = @{
+	packageName    = 'geforce-game-ready-driver'
+	fileType       = 'EXE'
+	url64          = 'https://us.download.nvidia.com/Windows/416.94/416.94-desktop-win10-64bit-international-whql.exe'
+	checksum64     = '2d779d173e6d87af1b481f49318145109dfd032993603925a1ddd97f6a02f6a8'
+	checksumType64 = 'sha256'
+	silentArgs     = '-s -noreboot'
+	validExitCodes = @(0,1)
+	softwareName   = 'NVIDIA Graphics Driver*'
+}
+
+If ( [System.Environment]::OSVersion.Version.Major -ne '10' ) {
+	$packageArgs['url64']      = 'https://us.download.nvidia.com/Windows/416.94/416.94-desktop-win8-win7-64bit-international-whql.exe'
+	$packageArgs['checksum64'] = '6a65ebcd1635cdcc8b9ca0364d3811ae382c7e786ae0f46ea8b047d606dbed9b'
+}
+
+If ( -not (Get-OSArchitectureWidth -compare 64) ) {
+	Write-Warning "NVIDIA has ended support for 32bit operating systems."
+	Write-Warning "32 bit users should specify version 391.35."
+	Write-Warning "Security patches for 32bit may be available on geforce.com"
+	Write-Error "32 bit no longer supported."
+}
+
+Install-ChocolateyPackage @packageArgs
+
+Write-Host "The package 'nvidia-display-driver' is also available for those who don't want or need the extra software bundled with the conventional geforce package."
---
+++ geforce-game-ready-driver-notebook/tools/chocolateyuninstall.ps1
@@ -0,0 +1,18 @@
+﻿$ErrorActionPreference = 'Stop';
+$packageArgs = @{
+  packageName   = 'geforce-game-ready-driver'
+  softwareName  = 'NVIDIA Graphics Driver*'
+  fileType      = 'EXE'
+  validExitCodes= @(0)
+}
+
+$packageArgs['file']       = (Get-UninstallRegistryKey $packageArgs['softwareName']).UninstallString -replace "\s.*$"
+$packageArgs['silentArgs'] = (Get-UninstallRegistryKey $packageArgs['softwareName']).UninstallString -replace "^.*?\s" -replace "$"," -silent"
+
+If ( -not $env:ChocolateyForce) {
+  Write-Warning 'Uninstalling this package will forcibly reboot your PC.'
+  Write-Warning 'If you wish to proceed, please specify the --force parameter.'
+  Write-Error 'Uninstall not called with --force. Aborting.'
+}
+
+Uninstall-ChocolateyPackage @packageArgs
