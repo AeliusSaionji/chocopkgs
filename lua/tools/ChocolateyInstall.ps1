ff21e2f2ae3b208b4e2dae9fc6ff9f01f024a7e5
Aelius <4342746+AeliusSaionji@users.noreply.github.com>
2018-12-01 14:20:01 -0500

    update luaforwindows

--- lua/tools/ChocolateyInstall.ps1
+++ lua/tools/ChocolateyInstall.ps1
@@ -1,1 +1,3 @@
-Install-ChocolateyPackage 'lua' 'exe' '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-' 'https://github.com/rjpcomputing/luaforwindows/releases/download/v5.1.4-50/LuaForWindows_v5.1.4-50.exe'
+Install-ChocolateyPackage -PackageName 'lua' -FileType 'exe' -SilentArgs '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-' `
+-Url 'https://github.com/rjpcomputing/luaforwindows/releases/download/v5.1.5-52/LuaForWindows_v5.1.5-52.exe' `
+-Checksum c5f96e5183adea82a0f0d25ad0940c7ae50d40c542d2d7331cc887d5fddb4191 -ChecksumType sha256
