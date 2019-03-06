df97ae3d7c4f51dd31b3aa9ebe40ab6b3d5be2f8
Chocolatey <chocolatey@realdimensions.net>
2019-03-06 00:09:04 +0000

    AU: 2 updated - geforce-game-ready-driver nvidia-display-driver [skip ci]

--- nvidia-display-driver/tools/chocolateyInstall.ps1
+++ nvidia-display-driver/tools/chocolateyInstall.ps1
@@ -3,8 +3,8 @@ $ErrorActionPreference = 'Stop';
 $packageArgs = @{
 	packageName    = 'nvidia-display-driver'
 	destination    = "${ENV:TEMP}\nvidiadriver" # Folder to extract drivers
-	url64          = 'https://us.download.nvidia.com/Windows/419.17/419.17-desktop-win10-64bit-international-whql.exe'
-	checksum64     = 'bdb94b8f0c5ff800d83a55f6e84aa8b7764fcf972eb2c897084b5bbde5a1c8c5'
+	url64          = 'https://us.download.nvidia.com/Windows/419.35/419.35-desktop-win10-64bit-international-whql.exe'
+	checksum64     = 'ebb9ee4ecf9363b081ad7d85c595d4d24a331f59303cb5329e19641c6d132966'
 	checksumType64 = 'sha256'
 	silentArgs     = '-s -noreboot'
 	validExitCodes = @(0,1)
@@ -12,8 +12,8 @@ $packageArgs = @{
 }
 
 If ( [System.Environment]::OSVersion.Version.Major -ne '10' ) {
-	$packageArgs['url64']      = 'https://us.download.nvidia.com/Windows/419.17/419.17-desktop-win8-win7-64bit-international-whql.exe'
-	$packageArgs['checksum64'] = '9dc12609f010487f5f27c0d19988519eb81788405d143bcd02fb7028d02f7c93'
+	$packageArgs['url64']      = 'https://us.download.nvidia.com/Windows/419.35/419.35-desktop-win8-win7-64bit-international-whql.exe'
+	$packageArgs['checksum64'] = '7a315214e1843c82ed87c5ebb0dff3c2f34901131988aa0a633c2a147dfc1e69'
 }
 
 If ( -not (Get-OSArchitectureWidth -compare 64) ) {
