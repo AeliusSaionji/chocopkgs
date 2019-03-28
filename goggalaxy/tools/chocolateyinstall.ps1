c3d77190047feb565f8c181dee8aa54350b114a9
aelius <aeliussaionji@outlook.com>
2019-03-28 03:51:23 -0400

    initial goggalaxy package

---
+++ goggalaxy/tools/chocolateyinstall.ps1
@@ -0,0 +1,13 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  fileType       = 'EXE'
+  url32          = ''
+  softwareName   = 'GOG Galaxy*'
+  checksum32     = ''
+  checksumType32 = 'sha256'
+  silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+}
+
+Install-ChocolateyPackage @packageArgs
---
+++ goggalaxy/update.ps1
@@ -0,0 +1,27 @@
+Import-Module au
+
+$releases = 'https://www.gog.com/galaxy'
+
+function global:au_SearchReplace {
+	@{
+		".\tools\chocolateyInstall.ps1" = @{
+			"(?i)(^\s*url32\s*=\s*)('.*')"          = "`$1'$($Latest.URL32)'"
+			"(?i)(^\s*checksumType32\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType32)'"
+			"(?i)(^\s*checksum32\s*=\s*)('.*')"     = "`$1'$($Latest.Checksum32)'"
+		}
+	}
+}
+
+function global:au_BeforeUpdate() {}
+
+function global:au_GetLatest {
+	$url32 = iwr $releases -UseBasicParsing | Where-Object {$_ -like '*exe'} | Select-Object -First 1
+	$url32 -match '\d+\.\d+\.\d+\.\d+'
+	$version = $Matches[0]
+
+	return @{ Version = $version; URL32 = $url32 }
+}
+
+if ($MyInvocation.InvocationName -ne '.') { # run the update only if script is not sourced
+	Update-Package
+}
