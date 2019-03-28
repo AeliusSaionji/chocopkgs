436a9d0c6a793eccedb2463e2cd95bfee40479d8
aelius <aeliussaionji@outlook.com>
2019-03-28 04:06:44 -0400

    updatefixes and update run

--- goggalaxy/tools/chocolateyinstall.ps1
+++ goggalaxy/tools/chocolateyinstall.ps1
@@ -1,12 +1,12 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $packageArgs = @{
-  packageName    = $env:ChocolateyPackageName
-  fileType       = 'EXE'
-  url32          = ''
-  softwareName   = 'GOG Galaxy*'
-  checksum32     = ''
-  checksumType32 = 'sha256'
+  packageName  = $env:ChocolateyPackageName
+  fileType     = 'EXE'
+  url          = 'https://content-system.gog.com/open_link/download?path=/open/galaxy/client/setup_galaxy_1.2.54.23.exe'
+  softwareName = 'GOG Galaxy*'
+  checksum     = '423a465f98350a2c16abe1dfcf56b5fab7e5683f82d96ea055665b5e3316e911'
+  checksumType = 'sha256'
   silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
 }
 
--- goggalaxy/update.ps1
+++ goggalaxy/update.ps1
@@ -5,9 +5,9 @@ $releases = 'https://www.gog.com/galaxy'
 function global:au_SearchReplace {
 	@{
 		".\tools\chocolateyInstall.ps1" = @{
-			"(?i)(^\s*url32\s*=\s*)('.*')"          = "`$1'$($Latest.URL32)'"
-			"(?i)(^\s*checksumType32\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType32)'"
-			"(?i)(^\s*checksum32\s*=\s*)('.*')"     = "`$1'$($Latest.Checksum32)'"
+			"(?i)(^\s*url\s*=\s*)('.*')"          = "`$1'$($Latest.URL32)'"
+			"(?i)(^\s*checksumType\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType32)'"
+			"(?i)(^\s*checksum\s*=\s*)('.*')"     = "`$1'$($Latest.Checksum32)'"
 		}
 	}
 }
@@ -15,7 +15,7 @@ function global:au_SearchReplace {
 function global:au_BeforeUpdate() {}
 
 function global:au_GetLatest {
-	$url32 = iwr $releases -UseBasicParsing | Where-Object {$_ -like '*exe'} | Select-Object -First 1
+	$url32 = (iwr $releases -UseBasicParsing).Links.href | Where-Object {$_ -like '*exe'} | Select-Object -First 1
 	$url32 -match '\d+\.\d+\.\d+\.\d+'
 	$version = $Matches[0]
 
