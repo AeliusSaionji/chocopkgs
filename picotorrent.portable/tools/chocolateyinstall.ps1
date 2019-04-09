f395c8bed5406ec074b8b5b5f770f58740c246ea
aelius <aeliussaionji@outlook.com>
2019-04-09 14:18:34 -0400

    add picotorrent portable, add screenshot to all picotorrent

---
+++ picotorrent.portable/tools/chocolateyinstall.ps1
@@ -0,0 +1,17 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$fileLocation   = Get-Item $toolsDir\*_x32.zip
+$file64Location = Get-Item $toolsDir\*_x64.zip
+$destination = "$(Get-ToolsLocation)\PicoTorrent"
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $destination
+  file          = $fileLocation
+  file64        = $file64Location
+}
+
+Install-ChocolateyZipPackage @packageArgs
+Remove-Item $packageArgs.file   -Force -ea 0
+Remove-Item $packageArgs.file64 -Force -ea 0
+Install-BinFile -Name "PicoTorrent" -Path "$destination\PicoTorrent.exe" -UseStart
