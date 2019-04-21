bc6bb336c57a8e45e4cd1e5c437c66ef9181e008
aelius <aeliussaionji@outlook.com>
2019-04-20 21:57:23 -0400

    add shortcuts to picotorrent portable

--- picotorrent.portable/tools/chocolateyinstall.ps1
+++ picotorrent.portable/tools/chocolateyinstall.ps1
@@ -4,14 +4,24 @@ $fileLocation   = Get-Item $toolsDir\*_x32.zip
 $file64Location = Get-Item $toolsDir\*_x64.zip
 $destination = "$(Get-ToolsLocation)\PicoTorrent"
 
+# Place shortcuts in appropriate location
+$ProgsFolder = [environment]::getfolderpath('Programs')
+If ( Test-ProcessAdminRights ) {
+  $ProgsFolder = Join-Path ([environment]::getfolderpath('CommonApplicationData')) "Microsoft\Windows\Start Menu\Programs"
+}
+
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $destination
   file          = $fileLocation
   file64        = $file64Location
+  shortcutFilePath = "$ProgsFolder\PicoTorrent.lnk"
+  targetPath       = "$destination\PicoTorrent.exe"
+  WorkingDirectory = "$destination\"
 }
 
 Install-ChocolateyZipPackage @packageArgs
+Install-ChocolateyShortcut @packageArgs
+Install-BinFile -Name "PicoTorrent" -Path "$destination\PicoTorrent.exe" -UseStart
 Remove-Item $packageArgs.file   -Force -ea 0
 Remove-Item $packageArgs.file64 -Force -ea 0
-Install-BinFile -Name "PicoTorrent" -Path "$destination\PicoTorrent.exe" -UseStart
--- picotorrent.portable/tools/chocolateyuninstall.ps1
+++ picotorrent.portable/tools/chocolateyuninstall.ps1
@@ -1,1 +1,5 @@
-﻿Uninstall-BinFile -Name "PicoTorrent"
+﻿$ErrorActionPreference = 'Stop'
+$commProgs = Join-Path ([environment]::getfolderpath('CommonApplicationData')) "Microsoft\Windows\Start Menu\Programs"
+$userProgs = [environment]::getfolderpath('Programs')
+Remove-Item -Path "$commProgs\PicoTorrent.lnk","$userProgs\PicoTorrent.lnk" -ea 0
+Uninstall-BinFile -Name "PicoTorrent"
