049fd19a1a8fcf8d88d8f0154b21f91cf3314262
aelius <git@flamingsteel.net>
2019-07-02 16:04:52 -0400

    initial plexmediaplayer package

---
+++ plexmediaplayer/tools/chocolateyinstall.ps1
@@ -0,0 +1,14 @@
+﻿# https://www.plex.tv/media-server-downloads/#plex-app
+$ErrorActionPreference = 'Stop'
+
+$packageArgs = @{
+  packageName    = 'plexmediaplayer'
+  fileType       = 'EXE'
+  url64          = ''
+  silentArgs     = '/S'
+  softwareName   = 'Plex Media Player' 
+  checksum64     = ''
+  checksumType64 = ''  
+}
+
+Install-ChocolateyPackage @packageArgs
