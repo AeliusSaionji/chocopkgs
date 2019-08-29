ca336b888ee0fa99062b33b3c11c602ec3161aae
aelius <git@flamingsteel.net>
2019-08-29 04:02:10 -0400

    Plex: add a valid exit code

--- plex/tools/chocolateyinstall.ps1
+++ plex/tools/chocolateyinstall.ps1
@@ -4,11 +4,12 @@ $ErrorActionPreference = 'Stop'
 $packageArgs = @{
   packageName    = 'plex'
   fileType       = 'EXE'
-  url64          = 'https://downloads.plex.tv/plex-desktop/1.0.0.792-d4adb056/windows/Plex-1.0.0.792-d4adb056-x86_64.exe'
+  url64          = 'https://downloads.plex.tv/plex-desktop/1.2.0.875-b7362913/windows/Plex-1.2.0.875-b7362913-x86_64.exe'
   silentArgs     = '/S'
   softwareName   = 'Plex' 
-  checksum64     = 'EADE2911FB39E5EDB1A53AD04312DD8F1386AC98A4EF3B845407176A36AFE16F'
+  checksum64     = 'a40efe64153fdc63e297996da9e03272e2df228acd526e13a58ba18c67d0ddc1'
   checksumType64 = 'sha256'  
+  validExitCodes = @(0, 1223)
 }
 
 Install-ChocolateyPackage @packageArgs
