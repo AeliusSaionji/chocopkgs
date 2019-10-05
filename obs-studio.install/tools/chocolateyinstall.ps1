e06a54b19e71c8c1ad8c57d43b586333c6a44678
TheCakeIsNaOH <38865330+TheCakeIsNaOH@users.noreply.github.com>
2019-10-05 10:03:11 -0500

    OBS Studio add pameter for no desktop shortcut.

--- obs-studio.install/tools/chocolateyinstall.ps1
+++ obs-studio.install/tools/chocolateyinstall.ps1
@@ -2,6 +2,9 @@
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $fileLocation   = Get-Item $toolsDir\*_x32.exe
 $file64Location = Get-Item $toolsDir\*_x64.exe
+$pp             = Get-PackageParameters
+$shortcutName   = 'OBS Studio.lnk'
+$shortcut       = Join-Path ([Environment]::GetFolderPath("CommonDesktopDirectory")) $shortcutName
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
@@ -15,3 +18,12 @@ $packageArgs = @{
 Install-ChocolateyInstallPackage @packageArgs
 Remove-Item $packageArgs.file   -Force -ea 0
 Remove-Item $packageArgs.file64 -Force -ea 0
+
+if ($pp['noicon']) {
+	if (Test-Path $shortcut) {
+		Remove-Item $shortcut
+		Write-Host -ForegroundColor green 'Removed ' $shortcut
+	} else {
+		Write-Host -ForegroundColor yellow 'Did not find ' $shortcut
+	}
+}
