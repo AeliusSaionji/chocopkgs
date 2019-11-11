a8ce706cd035799e37a0788ae79ad5883bd0bede
aelius <aeliussaionji@outlook.com>
2019-11-11 13:35:31 -0500

    update mpv.portable, major changes

--- mpv.portable/tools/chocolateyinstall.ps1
+++ mpv.portable/tools/chocolateyinstall.ps1
@@ -1,10 +1,17 @@
-﻿$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+﻿$ErrorActionPreference = 'Stop'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
     packageName = 'mpv.install'
-    file        = gi $toolsDir\*_x32.7z
-    file64      = gi $toolsDir\*_x64.7z
+    file        = gi "$toolsDir\*i686.7z"
+    file64      = gi "$toolsDir\*x86_64.7z"
     destination = "$toolsDir"
+}
+
+If ( Get-Item $toolsDir\doc\mpv.html -ea 0) {
+	Write-Warning 'For technical reasons, please uninstall then reinstall mpv.'
+	Write-Warning 'cuninst mpv.install; cinst mpv.install'
+	Write-Error 'The builds provided by this package have changed and mpv needs to be reinstalled.'
 }
 
 Get-ChocolateyUnzip @packageArgs
