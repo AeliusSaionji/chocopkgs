49c7646bf6b3c70e67e57887ecb1f2a5277ca959
aelius <aeliussaionji@outlook.com>
2019-11-12 16:02:02 -0500

    qimgv initial commit

---
+++ qimgv/tools/chocolateyinstall.ps1
@@ -0,0 +1,19 @@
+﻿$ErrorActionPreference = 'Stop'; # stop on all errors
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$fileLocation   = Get-Item $toolsDir\*_x32.exe
+$file64Location = Get-Item $toolsDir\*_x64.exe
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'EXE'
+  file          = $fileLocation
+  file64        = $file64Location
+  softwareName  = 'qimgv*'
+  silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+  validExitCodes= @(0)
+}
+
+Install-ChocolateyInstallPackage @packageArgs
+Remove-Item $packageArgs.file   -Force -ea 0
+Remove-Item $packageArgs.file64 -Force -ea 0
---
+++ qimgv/update.ps1
@@ -0,0 +1,34 @@
+﻿Import-Module au
+
+$releases = 'https://github.com/easymodo/qimgv/releases/latest'
+
+function global:au_SearchReplace {
+	@{
+		".\tools\VERIFICATION.txt" = @{
+			"(?i)(^\s*x32:).*"                  = "`${1} $($Latest.URL32)"
+			"(?i)(^\s*x64:).*"                  = "`${1} $($Latest.URL64)"
+			"(?i)(^\s*checksum\s*type\:).*"     = "`${1} $($Latest.ChecksumType32)"
+			"(?i)(^\s*checksum32\:).*"          = "`${1} $($Latest.Checksum32)"
+			"(?i)(^\s*checksum64\:).*"          = "`${1} $($Latest.Checksum64)"
+		}
+	}
+}
+function global:au_BeforeUpdate() {
+	Get-RemoteFiles -Purge
+}
+
+function global:au_GetLatest {
+	$cache_page = (iwr $releases -UseBasicParsing).Links.href
+	$download_page = $cache_page | Select-String '/tag/' | Select-Object -First 1
+	$Matches = $null
+	$download_page -match '\d+\.\d+\.\d+'
+	$version = $Matches[0]
+	$url32 = 'https://github.com' + ($cache_page | Select-String 'qimgv_[0-9\.]+-video.exe$')
+	$url64 = 'https://github.com' + ($cache_page | Select-String 'qimgv-x64_[0-9\.]+-video.exe$')
+
+	return @{ Version = $version; URL32 = $url32; URL64 = $url64; }
+}
+
+if ($MyInvocation.InvocationName -ne '.') { # run the update only if script is not sourced
+	Update-Package -checksumfor none
+}
