d29ba38182c4d4e95c1a6d638e3263086c0b2891
aelius <aeliussaionji@outlook.com>
2019-11-13 00:06:58 -0500

    qimgv portable initial commit

---
+++ qimgv.portable/tools/chocolateyinstall.ps1
@@ -0,0 +1,28 @@
+﻿$ErrorActionPreference = 'Stop'; # stop on all errors
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$fileLocation   = Get-Item $toolsDir\*_x32.zip
+$file64Location = Get-Item $toolsDir\*_x64.zip
+$destination = "$(Get-ToolsLocation)\qimgv"
+
+# Place shortcuts in appropriate location
+$ProgsFolder = [environment]::getfolderpath('Programs')
+If ( Test-ProcessAdminRights ) {
+  $ProgsFolder = Join-Path ([environment]::getfolderpath('CommonApplicationData')) "Microsoft\Windows\Start Menu\Programs"
+}
+
+$packageArgs = @{
+  packageName      = $env:ChocolateyPackageName
+  unzipLocation    = $destination
+  file             = $fileLocation
+  file64           = $file64Location
+  shortcutFilePath = "$ProgsFolder\qimgv.lnk"
+  targetPath       = "$destination\qimgv.exe"
+  WorkingDirectory = "$destination\"
+}
+
+Install-ChocolateyZipPackage @packageArgs
+Install-ChocolateyShortcut @packageArgs
+Install-BinFile -Name "qimgv" -Path "$destination\qimgv.exe" -UseStart
+Install-ChocolateyInstallPackage @packageArgs
+Remove-Item $packageArgs.file   -Force -ea 0
+Remove-Item $packageArgs.file64 -Force -ea 0
