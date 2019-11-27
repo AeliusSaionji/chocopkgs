899e83b69a9637d3c5a1c17b125cb0cd098c4773
Chocolatey <chocolatey@realdimensions.net>
2019-11-27 00:11:07 +0000

    AU: 2 updated - geforce-game-ready-driver nvidia-display-driver [skip ci]

--- geforce-game-ready-driver/tools/chocolateyInstall.ps1
+++ geforce-game-ready-driver/tools/chocolateyInstall.ps1
@@ -2,8 +2,8 @@
 $packageArgs = @{
 	packageName    = 'geforce-game-ready-driver'
 	fileType       = 'EXE'
-	url64          = 'https://us.download.nvidia.com/Windows/441.20/441.20-desktop-win10-64bit-international-whql.exe'
-	checksum64     = '696bafbb02d8459bb61f15891801701a917af4c9892f16db56e1d5a7b3b35e56'
+	url64          = 'https://us.download.nvidia.com/Windows/441.41/441.41-desktop-win10-64bit-international-whql.exe'
+	checksum64     = '3f30c38f083834ae2f5867f4ca6cc7487b1b1c145305943cc81391aa02641d88'
 	checksumType64 = 'sha256'
 	silentArgs     = '-s -noreboot'
 	validExitCodes = @(0,1)
@@ -11,14 +11,14 @@ $packageArgs = @{
 }
 
 If ( [System.Environment]::OSVersion.Version.Major -ne '10' ) {
-	$packageArgs['url64']      = 'https://us.download.nvidia.com/Windows/441.20/441.20-desktop-win8-win7-64bit-international-whql.exe'
-	$packageArgs['checksum64'] = 'aa24f3f8ed7838eff3f9e11c582cfc53bf04891a7766021378adf36966c492a2'
+	$packageArgs['url64']      = 'https://us.download.nvidia.com/Windows/441.41/441.41-desktop-win8-win7-64bit-international-whql.exe'
+	$packageArgs['checksum64'] = 'f0bae56e7e327dc59387d8b06e0a33eabba5f7a5ac0b9b190cb82b0628a898bf'
 }
 
 $pp = Get-PackageParameters
 If ($pp['dch'] -eq 'true') {
-	$packageArgsDCHURL      = 'https://us.download.nvidia.com/Windows/441.20/441.20-desktop-win10-64bit-international-dch-whql.exe'
-	$packageArgsDCHChecksum = '1a07047994e142f9458d4d258aeabbff1675d5507ee09d3b8d6c48b16220576f'
+	$packageArgsDCHURL      = 'https://us.download.nvidia.com/Windows/441.41/441.41-desktop-win10-64bit-international-dch-whql.exe'
+	$packageArgsDCHChecksum = '89d9625378f5a97a5eb04af41f8536837de0f31930c64d4c9455e3c990708576'
 	$packageArgs['url64']      = $packageArgsDCHURL
 	$packageArgs['checksum64'] = $packageArgsDCHChecksum
 }
