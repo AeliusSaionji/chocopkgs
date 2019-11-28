bf433aaf4435413567ae194352a655a9742a997b
aelius <aeliussaionji@outlook.com>
2019-11-28 02:37:38 -0500

    plex update

--- plex/tools/chocolateyinstall.ps1
+++ plex/tools/chocolateyinstall.ps1
@@ -4,10 +4,10 @@ $ErrorActionPreference = 'Stop'
 $packageArgs = @{
   packageName    = 'plex'
   fileType       = 'EXE'
-  url64          = 'https://downloads.plex.tv/plex-desktop/1.3.1.916-1cb2c34d/windows/Plex-1.3.1.916-1cb2c34d-x86_64.exe'
+  url64          = 'https://downloads.plex.tv/plex-desktop/1.4.1.940-574c2fa7/windows/Plex-1.4.1.940-574c2fa7-x86_64.exe'
   silentArgs     = '/S'
   softwareName   = 'Plex' 
-  checksum64     = '4d2fec5579b2d0ff9bf62ab872e07cff72f726736b4ca184098387dc56994f32'
+  checksum64     = '19F0959F0F7953AE40A3A0039DA85741B2F6381C4C662BCB47222EE11938E6B5'
   checksumType64 = 'sha256'  
   validExitCodes = @(0, 1223)
 }
