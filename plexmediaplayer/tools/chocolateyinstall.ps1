291410fd3514703983ab3709c58840e3f7ab892a
Chocolatey <chocolatey@realdimensions.net>
2020-01-08 00:06:31 +0000

    AU: 1 updated - plexmediaplayer [skip ci]

--- plexmediaplayer/tools/chocolateyinstall.ps1
+++ plexmediaplayer/tools/chocolateyinstall.ps1
@@ -4,10 +4,10 @@ $ErrorActionPreference = 'Stop'
 $packageArgs = @{
   packageName    = 'plexmediaplayer'
   fileType       = 'EXE'
-  url64          = 'https://downloads.plex.tv/plexmediaplayer/2.47.0.1035-e74d341b/PlexMediaPlayer-2.47.0.1035-e74d341b-windows-x64.exe'
+  url64          = 'https://downloads.plex.tv/plexmediaplayer/2.48.0.1038-11b21f57/PlexMediaPlayer-2.48.0.1038-11b21f57-windows-x64.exe'
   silentArgs     = '/S'
   softwareName   = 'Plex Media Player' 
-  checksum64     = '10b484478ee0ed8840a37e1b64bdca279422999570f6cd380f8e6854203e168e'
+  checksum64     = '2e39b297e87686ca0fd4835d1155761fa3224afdbf798f2d7f99b7e53fee32ff'
   checksumType64 = 'sha256'  
 }
 
