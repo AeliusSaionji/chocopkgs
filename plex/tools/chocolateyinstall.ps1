be13e641b6e7d80ea1a93fb8b0f0c3a746871b1f
aelius <git@flamingsteel.net>
2020-03-15 00:00:26 -0400

    plex manual updatplex manual updatee

--- plex/tools/chocolateyinstall.ps1
+++ plex/tools/chocolateyinstall.ps1
@@ -4,10 +4,10 @@ $ErrorActionPreference = 'Stop'
 $packageArgs = @{
   packageName    = 'plex'
   fileType       = 'EXE'
-  url64          = 'https://downloads.plex.tv/plex-desktop/1.4.1.940-574c2fa7/windows/Plex-1.4.1.940-574c2fa7-x86_64.exe'
+  url64          = 'https://downloads.plex.tv/plex-desktop/1.6.3.1009-57cf57c8/windows/Plex-1.6.3.1009-57cf57c8-x86_64.exe'
   silentArgs     = '/S'
   softwareName   = 'Plex' 
-  checksum64     = '19F0959F0F7953AE40A3A0039DA85741B2F6381C4C662BCB47222EE11938E6B5'
+  checksum64     = 'e187fa658aec86b12c8b906f65fcd03383f14735f2356ee5cec2e57ef1c45e23'
   checksumType64 = 'sha256'  
   validExitCodes = @(0, 1223)
 }
