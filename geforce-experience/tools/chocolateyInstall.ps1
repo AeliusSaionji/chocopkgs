5a239ec8ca26ddad857c9447ccd54a018a26922a
Chocolatey <chocolatey@realdimensions.net>
2020-04-14 12:07:50 +0000

    AU: 4 updated - geforce-experience vim-tux vim-tux.install vim-tux.portable [skip ci]

--- geforce-experience/tools/chocolateyInstall.ps1
+++ geforce-experience/tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
 ﻿$packageArgs = @{
     packageName    = 'geforce-experience'
     filetype       = 'EXE'
-    url            = 'https://us.download.nvidia.com/GFE/GFEClient/3.20.2.34/GeForce_Experience_v3.20.2.34.exe'
-    checksum       = 'aeddc7d03f74b6287f5c7484b95c945758a64f336278f89bad3dc995d3343d66'
+    url            = 'https://us.download.nvidia.com/GFE/GFEClient/3.20.3.63/GeForce_Experience_v3.20.3.63.exe'
+    checksum       = '07d377dfca43ebd198dcb8cc060e9cbd793da09f8533e2de4bf079e0b2d827ef'
     checksumType   = 'sha256'
     silentArgs     = '-s -noreboot'
     validExitCodes = @(0)
