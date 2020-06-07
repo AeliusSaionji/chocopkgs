3ed930b90c18cca63a0aa5332cb9c7f82dca9d03
aelius <email>
2020-06-07 18:56:17 -0400

    plex updateps1 based on newly dug up api

--- plex/tools/chocolateyinstall.ps1
+++ plex/tools/chocolateyinstall.ps1
@@ -4,11 +4,11 @@ $ErrorActionPreference = 'Stop'
 $packageArgs = @{
   packageName    = 'plex'
   fileType       = 'EXE'
-  url64          = 'https://downloads.plex.tv/plex-desktop/1.9.0.1188-c009e512/windows/Plex-1.9.0.1188-c009e512-x86_64.exe'
+  url64          = 'https://downloads.plex.tv/plex-desktop/1.10.0.1208-daa6b641/windows/Plex-1.10.0.1208-daa6b641-x86_64.exe'
   silentArgs     = '/S'
   softwareName   = 'Plex' 
-  checksum64     = '5c3d6506578602e9c1e17415b4f9dbdb91029bb23cd47c6d91f17c41816cdfb6'
-  checksumType64 = 'sha256'  
+  checksum64     = '5DA767FBE859D78C86B1B2EC151D7A824985ED234EA3A240770E17E26CF07741'
+  checksumType64 = 'sha1'  
   validExitCodes = @(0, 1223)
 }
 
---
+++ plex/update.ps1
@@ -0,0 +1,35 @@
+﻿Import-Module au
+
+$releases = "https://plex.tv/api/downloads/6.json"
+
+function global:au_SearchReplace {
+  @{
+    ".\tools\chocolateyinstall.ps1" = @{
+      "(?i)(^\s*url64\s*=).*"              = "`${1} '$($Latest.URL64)'"
+      "(?i)(^\s*checksum\s*Type64\s*=).*"  = "`${1} '$($Latest.ChecksumType64)'"
+      "(?i)(^\s*checksum64\s*=).*"         = "`${1} '$($Latest.Checksum64)'"
+    }
+  }
+}
+
+function global:au_BeforeUpdate() {
+  Get-RemoteFiles -Purge
+}
+
+function global:au_GetLatest {
+  $download_page = iwr $releases -UseBasicParsing | ConvertFrom-Json
+  # If the array has more than one element, that's probably a hotfix
+  $hotfix = 1
+  if ($download_page.computer.Windows.releases.count -gt 1) {
+    $hotfix = 0
+  }
+  $version = $download_page.computer.Windows.version -match '(\d+\.?)+'
+  $url64   = $download_page.computer.Windows.releases[-1].url
+  $sha1    = $download_page.computer.Windows.releases[-1].checksum
+
+  return @{ Version = $version; URL64 = $url64; Checksum64 = $sha1; ChecksumType64 = 'sha1'; Hotfix = $hotfix }
+}
+
+if ($MyInvocation.InvocationName -ne '.') { # run the update only if script is not sourced
+  Update-Package -checksumfor none
+}
