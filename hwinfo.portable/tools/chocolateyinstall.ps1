fecfedc100e4a4ce69fd6bae3a1cc8f2a45179b2
sirAndros <andros.osipkov@gmail.com>
2020-07-20 13:52:15 +0200

    fix GUI start from console

--- hwinfo.portable/tools/chocolateyinstall.ps1
+++ hwinfo.portable/tools/chocolateyinstall.ps1
@@ -9,4 +9,11 @@ $packageArgs = @{
 }
 
 Get-ChocolateyUnzip @packageArgs
+
+# create empty sidecar so shimgen creates shim for GUI rather than console
+Set-Content -Path (Join-Path -Path $toolsDir -ChildPath "HWiNFO32.exe.gui") `
+            -Value $null
+Set-Content -Path (Join-Path -Path $toolsDir -ChildPath "HWiNFO64.exe.gui") `
+            -Value $null
+
 Remove-Item -force "$toolsDir\*.zip" -ea 0
