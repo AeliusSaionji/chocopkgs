dc7a755754e5092f4cbd8041e7d84557214d2ed9
aelius <git@flamingsteel.net>
2020-08-31 23:29:48 -0400

    latencymon minor fix

--- latencymon/tools/chocolateyinstall.ps1
+++ latencymon/tools/chocolateyinstall.ps1
@@ -3,7 +3,7 @@
   fileType = 'EXE'
   SilentArgs = '/SP- /VERYSILENT /SUPPRESSMSGBOXES /NORESTART'
   url = 'http://www.resplendence.com/download/LatencyMon.exe'
-  checksum = 'e7c0125193a42530bc2f9004f67e62562a557399d14154e6a58cab68545ab08e'
+  checksum = 'e6c8fe0d2b4b03566e40a5e7a4474108f23df2f3709d677f8d925c7d858e5082'
   checksumType = 'SHA256'
 }
 
