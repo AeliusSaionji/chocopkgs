623d56b1b4826b78563f068c9e7dc2765d973e37
aelius <git@flamingsteel.net>
2020-08-31 23:09:08 -0400

    darktable initial commit

---
+++ darktable/tools/chocolateyinstall.ps1
@@ -0,0 +1,13 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$packageArgs = @{
+  packageName    = 'darktable'
+  fileType       = 'exe'
+  file64         = gi $toolsDir\*_x64.exe
+  softwareName   = 'darktable'
+  silentArgs     = '/S'
+}
+
+Install-ChocolateyInstallPackage @packageArgs
+Remove-Item $packageArgs.file64 -Force -ea 0
