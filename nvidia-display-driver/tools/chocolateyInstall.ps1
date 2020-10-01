50d050d03cbdb06ec6b5deef0d2a1a1443667cd1
aelius <git@flamingsteel.net>
2020-09-30 20:29:14 -0400

    nvidia tweaks

--- nvidia-display-driver/tools/chocolateyInstall.ps1
+++ nvidia-display-driver/tools/chocolateyInstall.ps1
@@ -1,34 +1,34 @@
 ﻿$instDir    = "${ENV:TEMP}\nvidiainstall" # Folder to move desired components into
 $ErrorActionPreference = 'Stop';
 $packageArgs = @{
-	packageName    = 'nvidia-display-driver'
-	destination    = "${ENV:TEMP}\nvidiadriver" # Folder to extract drivers
-	url64          = 'https://us.download.nvidia.com/Windows/456.55/456.55-desktop-win10-64bit-international-whql.exe'
-	checksum64     = '7e89635b47cf1cd7ad6d92d0271b0cd95855439fe57482faea24354830408eef'
-	checksumType64 = 'sha256'
-	silentArgs     = '-s -noreboot'
-	validExitCodes = @(0,1)
-	softwareName   = 'NVIDIA Graphics Driver*'
+  packageName    = 'nvidia-display-driver'
+  destination    = "${ENV:TEMP}\nvidiadriver" # Folder to extract drivers
+  url64          = 'https://us.download.nvidia.com/Windows/456.55/456.55-desktop-win10-64bit-international-whql.exe'
+  checksum64     = '7e89635b47cf1cd7ad6d92d0271b0cd95855439fe57482faea24354830408eef'
+  checksumType64 = 'sha256'
+  silentArgs     = '-s -noreboot'
+  validExitCodes = @(0,1)
+  softwareName   = 'NVIDIA Graphics Driver*'
 }
 
 If ( [System.Environment]::OSVersion.Version.Major -ne '10' ) {
-	$packageArgs['url64']      = 'https://us.download.nvidia.com/Windows/456.55/456.55-desktop-win8-win7-64bit-international-whql.exe'
-	$packageArgs['checksum64'] = '556df162757e296864c529c9420690fc96fd3f727d9800e3d73401511511dec5'
+  $packageArgs['url64']      = 'https://us.download.nvidia.com/Windows/456.55/456.55-desktop-win8-win7-64bit-international-whql.exe'
+  $packageArgs['checksum64'] = '556df162757e296864c529c9420690fc96fd3f727d9800e3d73401511511dec5'
 }
 
 $pp = Get-PackageParameters
 If ($pp['dch'] -eq 'true') {
-	$packageArgsDCHURL      = 'https://us.download.nvidia.com/Windows/456.55/456.55-desktop-win10-64bit-international-dch-whql.exe'
-	$packageArgsDCHChecksum = '7ddbc88c5babbbcf14ee662bc1ce3549ca87c76bc1c067a97da2720202da5723'
-	$packageArgs['url64']      = $packageArgsDCHURL
-	$packageArgs['checksum64'] = $packageArgsDCHChecksum
+  $packageArgsDCHURL      = 'https://us.download.nvidia.com/Windows/456.55/456.55-desktop-win10-64bit-international-dch-whql.exe'
+  $packageArgsDCHChecksum = '7ddbc88c5babbbcf14ee662bc1ce3549ca87c76bc1c067a97da2720202da5723'
+  $packageArgs['url64']      = $packageArgsDCHURL
+  $packageArgs['checksum64'] = $packageArgsDCHChecksum
 }
 
 If ( -not (Get-OSArchitectureWidth -compare 64) ) {
-	Write-Warning "NVIDIA has ended support for 32bit operating systems."
-	Write-Warning "32 bit users should specify version 391.35."
-	Write-Warning "Security patches for 32bit may be available on geforce.com"
-	Write-Error "32 bit no longer supported."
+  Write-Warning "NVIDIA has ended support for 32bit operating systems."
+  Write-Warning "32 bit users should specify version 391.35."
+  Write-Warning "Security patches for 32bit may be available on geforce.com"
+  Write-Error "32 bit no longer supported."
 }
 
 # Remove any previous tempfiles
@@ -56,15 +56,12 @@ Move-Item ($packageArgs['destination'] + "\setup.exe"             ) -Destination
 Move-Item ($packageArgs['destination'] + "\GFExperience\PrivacyPolicy"      ) -Destination "$instDir\GFExperience"
 Move-Item ($packageArgs['destination'] + "\GFExperience\EULA.html"          ) -Destination "$instDir\GFExperience"
 Move-Item ($packageArgs['destination'] + "\GFExperience\FunctionalConsent_*") -Destination "$instDir\GFExperience"
+# I've changed my mind about not including the audio and USB-C driver by default.
+Move-Item ($packageArgs['destination'] + "\HDAudio") -Destination "$instDir"
+Move-Item ($packageArgs['destination'] + "\PPC"    ) -Destination "$instDir" -ErrorAction SilentlyContinue
 if ( $pp.NV3DVision ) {
   Move-Item ($packageArgs['destination'] + "\NV3DVision"          ) -Destination "$instDir"
   Move-Item ($packageArgs['destination'] + "\NV3DVisionUSB.Driver") -Destination "$instDir"
-}
-if ( $pp.HDAudio ) {
-  Move-Item ($packageArgs['destination'] + "\HDAudio"             ) -Destination "$instDir"
-}
-if ( $pp.USBC ) {
-  Move-Item ($packageArgs['destination'] + "\PPC"                 ) -Destination "$instDir"
 }
 
 # Remove unused files
