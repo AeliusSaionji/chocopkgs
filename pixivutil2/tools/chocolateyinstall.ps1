d89fc725740dfdf6605aecaf8214fab069e667fa
Chocolatey <chocolatey@realdimensions.net>
2020-10-25 12:06:12 +0000

    AU: 1 updated - pixivutil2 [skip ci]

--- pixivutil2/tools/chocolateyinstall.ps1
+++ pixivutil2/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿$packageName = 'PixivUtil2'
 $unzipLocation = Join-Path "$(Get-ToolsLocation)" "$packageName"
-$url = 'https://github.com/Nandaka/PixivUtil2/releases/download/v20201024/pixivutil20201024.zip'
-$checksum = '59691f913f5b3c991d150b1d64028412ca60e009ae91e3602d4f73140fa47d51'
+$url = 'https://github.com/Nandaka/PixivUtil2/releases/download/v20201025/pixivutil20201025.zip'
+$checksum = '2e64e5a422f28cd57c2743d148ed9eec9c60924ab8f4d3d26d10fd8dde754319'
 $checksumType = 'sha256'
 
 Install-ChocolateyZipPackage $packageName $url $unzipLocation `
