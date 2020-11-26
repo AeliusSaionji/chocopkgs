6ea280fcc3345e14e7710cf5ed89566277b18b48
Chocolatey <chocolatey@realdimensions.net>
2020-11-26 00:08:03 +0000

    AU: 3 updated - goggalaxy hydrus-network plex [skip ci]

--- plex/tools/chocolateyinstall.ps1
+++ plex/tools/chocolateyinstall.ps1
@@ -4,10 +4,10 @@ $ErrorActionPreference = 'Stop'
 $packageArgs = @{
   packageName    = 'plex'
   fileType       = 'EXE'
-  url64          = 'https://downloads.plex.tv/plex-desktop/1.23.0.1435-e10e14cf/windows/Plex-1.23.0.1435-e10e14cf-x86_64.exe'
+  url64          = 'https://downloads.plex.tv/plex-desktop/1.24.0.1483-714cba36/windows/Plex-1.24.0.1483-714cba36-x86_64.exe'
   silentArgs     = '/S'
   softwareName   = 'Plex' 
-  checksum64     = '6779bdae7a0899f22c3f2cec7cb80333409db5fa'
+  checksum64     = '74578e8c2dbea524b3723efd3a7d1cba5a5e2b0b'
   checksumType64 = 'sha1'
   validExitCodes = @(0, 1223)
 }
