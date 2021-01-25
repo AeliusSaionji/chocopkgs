c7b4b8525f1e67edf8fff82475b185b13212b0b2
Chocolatey <chocolatey@realdimensions.net>
2021-01-25 12:06:02 +0000

    AU: 1 updated - geforce-experience [skip ci]

--- geforce-experience/tools/chocolateyInstall.ps1
+++ geforce-experience/tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
 ﻿$packageArgs = @{
     packageName    = 'geforce-experience'
     filetype       = 'EXE'
-    url            = 'https://us.download.nvidia.com/GFE/GFEClient/3.20.5.70/GeForce_Experience_v3.20.5.70.exe'
-    checksum       = '289b0ea5e737734217830349d4b9e6c510aa01bdd7e765efc75342ce21be85e4'
+    url            = 'https://us.download.nvidia.com/GFE/GFEClient/3.21.0.33/GeForce_Experience_v3.21.0.33.exe'
+    checksum       = '3c222fe4b67f433fed0738f7a3756582eadbb6c7b05de8e4de1020904414e668'
     checksumType   = 'sha256'
     silentArgs     = '-s -noreboot'
     validExitCodes = @(0)
