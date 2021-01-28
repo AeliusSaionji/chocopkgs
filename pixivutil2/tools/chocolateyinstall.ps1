bd53c7859e299450140290cc80169ea72f3bcd43
Chocolatey <chocolatey@realdimensions.net>
2021-01-28 12:07:19 +0000

    AU: 7 updated - pixivutil2 qutebrowser qutebrowser.install qutebrowser.portable vim-tux vim-tux.install vim-tux.portable [skip ci]

--- pixivutil2/tools/chocolateyinstall.ps1
+++ pixivutil2/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿$packageName = 'PixivUtil2'
 $unzipLocation = Join-Path "$(Get-ToolsLocation)" "$packageName"
-$url = 'https://github.com/Nandaka/PixivUtil2/releases/download/v20201130/pixivutil20201130.zip'
-$checksum = 'f5a65f6e3fc4bf23e79c2d0ef10aaf94b5fdadaaf32b9c0f0b074a3e9bbe2bfd'
+$url = 'https://github.com/Nandaka/PixivUtil2/releases/download/20210128/pixivutil20210128.zip'
+$checksum = 'fcae416cc9768158b9f77f1095d983f63050d2ab0ce310242c0fee9f1e6c7e8d'
 $checksumType = 'sha256'
 
 Install-ChocolateyZipPackage $packageName $url $unzipLocation `
