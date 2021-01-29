aa3486213c276a507c47ac2f03b002774caa2005
aelius <email>
2021-01-28 21:33:35 -0500

    latencymon rehash and tabularize

--- latencymon/tools/chocolateyinstall.ps1
+++ latencymon/tools/chocolateyinstall.ps1
@@ -1,9 +1,9 @@
 ﻿$packageArgs = @{
-  PackageName = 'LatencyMon'
-  fileType = 'EXE'
-  SilentArgs = '/SP- /VERYSILENT /SUPPRESSMSGBOXES /NORESTART'
-  url = 'http://www.resplendence.com/download/LatencyMon.exe'
-  checksum = 'e6c8fe0d2b4b03566e40a5e7a4474108f23df2f3709d677f8d925c7d858e5082'
+  PackageName  = 'LatencyMon'
+  fileType     = 'EXE'
+  SilentArgs   = '/SP- /VERYSILENT /SUPPRESSMSGBOXES /NORESTART'
+  url          = 'https://www.resplendence.com/download/LatencyMon.exe'
+  checksum     = '77E90F0F042F9806639DE25C67FA78948696CB635C8F47C4F11E92CD6162A2AE'
   checksumType = 'SHA256'
 }
 
