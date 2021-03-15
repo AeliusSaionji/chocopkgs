bcfd7d40e6aac91a5f973d65868f66a7e36a3362
aelius <git@flamingsteel.net>
2021-03-14 20:38:36 -0400

    goggalaxy use invoke-rest for rest, sanitize version, retab

--- goggalaxy/update.ps1
+++ goggalaxy/update.ps1
@@ -1,28 +1,31 @@
 Import-Module au
 
 $releases = 'https://remote-config.gog.com/components/webinstaller?component_version=2.0.0'
+$headers = @{}
 
 function global:au_SearchReplace {
-	@{
-		".\tools\chocolateyInstall.ps1" = @{
-			"(?i)(^\s*url\s*=\s*)('.*')"          = "`$1'$($Latest.URL32)'"
-			"(?i)(^\s*checksumType\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType32)'"
-			"(?i)(^\s*checksum\s*=\s*)('.*')"     = "`$1'$($Latest.Checksum32)'"
-		}
-	}
+  @{
+    ".\tools\chocolateyInstall.ps1" = @{
+      "(?i)(^\s*url\s*=\s*)('.*')"          = "`$1'$($Latest.URL32)'"
+      "(?i)(^\s*checksumType\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType32)'"
+      "(?i)(^\s*checksum\s*=\s*)('.*')"     = "`$1'$($Latest.Checksum32)'"
+    }
+  }
 }
 
 function global:au_BeforeUpdate() {}
 
 function global:au_GetLatest {
-	$download_page = iwr $releases -UseBasicParsing | ConvertFrom-Json
-	$version  = $download_page.content.windows.version
-	$url32    = $download_page.content.windows.downloadLink
-	$md5      = $download_page.content.windows.installerMd5
+  $restAPI = Invoke-RestMethod $releases -Headers $headers
+  $Matches = $null
+  $restAPI.content.windows.version -match '(\d+\.?)+'
+  $version = $Matches[0]
+  $url32   = $restAPI.content.windows.downloadLink
+  $md5     = $restAPI.content.windows.installerMd5
 
-	return @{ Version = $version; URL32 = $url32; Checksum32 = $md5; ChecksumType32 = 'md5' }
+  return @{ Version = $version; URL32 = $url32; Checksum32 = $md5; ChecksumType32 = 'md5' }
 }
 
 if ($MyInvocation.InvocationName -ne '.') { # run the update only if script is not sourced
-	Update-Package -checksumfor none
+  Update-Package -checksumfor none
 }
