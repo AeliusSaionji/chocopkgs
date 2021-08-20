ce3a3f47d46b4f5cfc7f0678172733ec28333be5
Chocolatey <chocolatey@realdimensions.net>
2021-08-20 00:07:23 +0000

    AU: 4 updated - goggalaxy vim-tux vim-tux.install vim-tux.portable [skip ci]

--- goggalaxy/tools/chocolateyinstall.ps1
+++ goggalaxy/tools/chocolateyinstall.ps1
@@ -3,9 +3,9 @@
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   fileType      = 'EXE'
-  url           = 'https://content-system.gog.com/open_link/download?path=/open/galaxy/client/2.0.40.1/setup_galaxy_2.0.40.1.exe'
+  url           = 'https://content-system.gog.com/open_link/download?path=/open/galaxy/client/2.0.41.150/setup_galaxy_2.0.41.150.exe'
   softwareName  = 'GOG Galaxy*'
-  checksum      = '977bf6c4b49db1ae9323ff12ffeced21'
+  checksum      = '311abdac40068965500dfb1d55eeb3be'
   checksumType  = 'md5'
   silentArgs    = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
   validExitCodes= @(0,1) # Code 1 - Same or newer version already installed. Uninstall before trying to downgrade.
