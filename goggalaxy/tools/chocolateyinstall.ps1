ac33fd8ce935d4d41ddcb842d26c46a4e2e7b6b3
Chocolatey <chocolatey@realdimensions.net>
2021-09-30 12:06:52 +0000

    AU: 2 updated - goggalaxy hydrus-network [skip ci]

--- goggalaxy/tools/chocolateyinstall.ps1
+++ goggalaxy/tools/chocolateyinstall.ps1
@@ -3,9 +3,9 @@
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   fileType      = 'EXE'
-  url           = 'https://content-system.gog.com/open_link/download?path=/open/galaxy/client/2.0.41.150/setup_galaxy_2.0.41.150.exe'
+  url           = 'https://content-system.gog.com/open_link/download?path=/open/galaxy/client/2.0.43.71/setup_galaxy_2.0.43.71a.exe'
   softwareName  = 'GOG Galaxy*'
-  checksum      = '311abdac40068965500dfb1d55eeb3be'
+  checksum      = 'b3d2865c4fe5f3352497b5f488939b0c'
   checksumType  = 'md5'
   silentArgs    = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
   validExitCodes= @(0,1) # Code 1 - Same or newer version already installed. Uninstall before trying to downgrade.
