d7bd5244bbf79e86c4824d50a0ece4f2cab174b4
Chocolatey <chocolatey@realdimensions.net>
2022-04-26 12:06:26 +0000

    AU: 1 updated - goggalaxy [skip ci]

--- goggalaxy/tools/chocolateyinstall.ps1
+++ goggalaxy/tools/chocolateyinstall.ps1
@@ -3,9 +3,9 @@
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   fileType      = 'EXE'
-  url           = 'https://content-system.gog.com/open_link/download?path=/open/galaxy/client/2.0.46.133/setup_galaxy_2.0.46.133.exe'
+  url           = 'https://content-system.gog.com/open_link/download?path=/open/galaxy/client/2.0.48.63/setup_galaxy_2.0.48.63.exe'
   softwareName  = 'GOG Galaxy*'
-  checksum      = '31c67f9322ebb6601ae7f485e0ffd5ed'
+  checksum      = '54c942f006ca347aa2d1b46a205248c9'
   checksumType  = 'md5'
   silentArgs    = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
   validExitCodes= @(0,1) # Code 1 - Same or newer version already installed. Uninstall before trying to downgrade.
