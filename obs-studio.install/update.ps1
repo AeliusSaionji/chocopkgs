2d24e1931d4325c46f272d6f0184628471aecd87
aelius <git@flamingsteel.net>
2022-10-13 01:35:30 -0400

    obs: updater broken, fixed with REST
    retabbed and pushed an update too

--- obs-studio.install/update.ps1
+++ obs-studio.install/update.ps1
@@ -1,32 +1,36 @@
 ﻿Import-Module au
 
-$releases = 'https://github.com/obsproject/obs-studio/releases/latest'
+$releases = 'https://api.github.com/repos/obsproject/obs-studio/releases/latest'
+$headers = @{
+    'User-Agent' = 'AeliusSaionji'
+    'Accept' = 'application/vnd.github.v3+json'
+}
 
 function global:au_SearchReplace {
-	@{
-		".\tools\VERIFICATION.txt" = @{
-			"(?i)(^\s*x64:).*"                  = "`${1} $($Latest.URL64)"
-			"(?i)(^\s*checksum\s*type\:).*"     = "`${1} $($Latest.ChecksumType64)"
-			"(?i)(^\s*checksum64\:).*"          = "`${1} $($Latest.Checksum64)"
-		}
-	}
+  @{
+    ".\tools\VERIFICATION.txt" = @{
+      "(?i)(^\s*x64:).*"                  = "`${1} $($Latest.URL64)"
+      "(?i)(^\s*checksum\s*type\:).*"     = "`${1} $($Latest.ChecksumType64)"
+      "(?i)(^\s*checksum64\:).*"          = "`${1} $($Latest.Checksum64)"
+    }
+  }
 }
 function global:au_BeforeUpdate() {
-	Get-RemoteFiles -Purge
+  Get-RemoteFiles -Purge
 }
 
 function global:au_GetLatest {
-	$cache_page = (iwr $releases -UseBasicParsing).Links.href
-	$download_page = $cache_page | Select-String '/tag/' | Select-Object -First 1
-	$Matches = $null
-	$download_page -match '\d+\.\d+\.\d+'
-	$version = $Matches[0]
-	# Filenames omit ending '0' in version string; just scrape again.
-	$url64 = 'https://github.com' + ($cache_page | Select-String 'x64.exe$' | Select-Object -First 1 )
+  $restAPI = Invoke-RestMethod $releases -Headers $headers
+  $Matches = $null
+  $restAPI.tag_name -match '(\d+\.?)+'
+  $version = $Matches[0]
+  $url64 = $restAPI.assets | Where-Object { ($_.content_type -eq 'application/x-msdownload') `
+    -and ($_.name -like '*Installer*') } `
+    | Select-Object -First 1 -ExpandProperty browser_download_url
 
-	return @{ Version = $version; URL64 = $url64; }
+  return @{ Version = $version; URL64 = $url64; }
 }
 
 if ($MyInvocation.InvocationName -ne '.') { # run the update only if script is not sourced
-	Update-Package -checksumfor none
+  Update-Package -checksumfor none
 }
