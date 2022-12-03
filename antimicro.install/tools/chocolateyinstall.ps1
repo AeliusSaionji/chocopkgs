58462e494c66e317cec420ce37ed18372c66006d
Chocolatey <chocolatey@realdimensions.net>
2022-12-03 00:10:10 +0000

    AU: 2 updated - antimicro.install vim-tux [skip ci]

--- antimicro.install/tools/chocolateyinstall.ps1
+++ antimicro.install/tools/chocolateyinstall.ps1
@@ -4,8 +4,8 @@
   softwareName   = 'AntiMicro'
   silentArgs     = '/qn /norestart'
   validExitCodes = @(0, 3010, 1641)
-  url            = 'https://github.com/AntiMicro/antimicro/releases/download/2.23/antimicro-2.23-win32.msi'
-  checksum       = '27817cc5c7673fc5e356db531325cfd0c95fa07b24d61c4f85363ae883627728'
+  url            = 'https://github.com/AntiMicro/antimicro/releases/download/2.24-final/antimicro-2.24-win64.msi'
+  checksum       = '6ed60e839281f6802dbbd2221079dcd9cd0562513f4124e8c431ab7a6e8d9af2'
   checksumType   = 'sha256'
 }
 
