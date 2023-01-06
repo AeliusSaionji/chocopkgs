3c9fdffb01099c9a6bcd65b18fc8383379989e3b
Chocolatey <chocolatey@realdimensions.net>
2023-01-06 00:09:52 +0000

    AU: 2 updated - geforce-game-ready-driver nvidia-display-driver [skip ci]

--- geforce-game-ready-driver/tools/chocolateyInstall.ps1
+++ geforce-game-ready-driver/tools/chocolateyInstall.ps1
@@ -2,8 +2,8 @@
 $packageArgs = @{
   packageName    = 'geforce-game-ready-driver'
   fileType       = 'EXE'
-  url64          = 'https://us.download.nvidia.com/Windows/527.56/527.56-desktop-win10-win11-64bit-international-dch-whql.exe'
-  checksum64     = '17f74d97a2037096fdd771c9d1c2d838878d6dcb6b394b67608cc5d6e8b9f520'
+  url64          = 'https://us.download.nvidia.com/Windows/528.02/528.02-desktop-win10-win11-64bit-international-dch-whql.exe'
+  checksum64     = '0d730ba6f058d35890e08c317f2921575a554226557777f651735f4f786c77eb'
   checksumType64 = 'sha256'
   silentArgs     = '-s -noreboot'
   validExitCodes = @(0,1)
@@ -11,8 +11,8 @@ $packageArgs = @{
 }
 
 If ( [System.Environment]::OSVersion.Version.Major -ne '10' ) {
-  $packageArgs['url64']      = 'https://us.download.nvidia.com/Windows/474.06/474.06-desktop-win8-win7-64bit-international-whql.exe'
-  $packageArgs['checksum64'] = 'bb4eabcecea63beb36c4f026ede9865433a6776a0f42c7a772b18f3d34bd0e57'
+  $packageArgs['url64']      = 'https://us.download.nvidia.com/Windows/474.11/474.11-desktop-win8-win7-64bit-international-whql.exe'
+  $packageArgs['checksum64'] = 'c0add59cbac27aad17d79c593cd5ef61a573783d5f69d8d5c99dc4ba3f8b7e0e'
 }
 
 If ( -not (Get-OSArchitectureWidth -compare 64) ) {
