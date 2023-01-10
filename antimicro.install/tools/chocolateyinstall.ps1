5c8adb5d4548ad930ee5499cf00b925d4cd2f372
aelius <git@flamingsteel.net>
2023-01-09 22:41:13 -0500

    antimicro final release update
    again

--- antimicro.install/tools/chocolateyinstall.ps1
+++ antimicro.install/tools/chocolateyinstall.ps1
@@ -1,12 +1,13 @@
-﻿$packageArgs = @{
+﻿$ErrorActionPreference = 'Stop';
+$packageArgs = @{
   packageName    = 'antimicro.install'
   fileType       = 'msi'
-  softwareName   = 'AntiMicro'
+  url64          = 'https://github.com/AntiMicro/antimicro/releases/download/2.24-final/antimicro-2.24-win64.msi'
+  checksum64     = '6ed60e839281f6802dbbd2221079dcd9cd0562513f4124e8c431ab7a6e8d9af2'
+  checksumType64 = 'sha256'
   silentArgs     = '/qn /norestart'
   validExitCodes = @(0, 3010, 1641)
-  url            = 'https://github.com/AntiMicro/antimicro/releases/download/2.24-final/antimicro-2.24-win64.msi'
-  checksum       = '6ed60e839281f6802dbbd2221079dcd9cd0562513f4124e8c431ab7a6e8d9af2'
-  checksumType   = 'sha256'
+  softwareName   = 'AntiMicro'
 }
 
 Install-ChocolateyPackage @packageArgs
--- antimicro.install/update.ps1
+++ antimicro.install/update.ps1
@@ -1,4 +1,4 @@
-Import-Module au
+﻿Import-Module au
 
 $releases = 'https://api.github.com/repos/AntiMicro/antimicro/releases/latest'
 $headers = @{
@@ -9,9 +9,9 @@ $headers = @{
 function global:au_SearchReplace {
   @{
     ".\tools\chocolateyinstall.ps1" = @{
-      "(?i)(^\s*[$]?checksumType\s*=\s*)('.*')" = "`${1}'$($Latest.ChecksumType32)'"
-      "(?i)(^\s*[$]?checksum\s*=\s*)('.*')"     = "`${1}'$($Latest.Checksum32)'"
-      "(?i)(^\s*[$]?url\s*=\s*)('.*')"          = "`${1}'$($Latest.URL32)'"
+      "(?i)(^\s*[$]?checksumType64\s*=\s*)('.*')" = "`${1}'$($Latest.ChecksumType64)'"
+      "(?i)(^\s*[$]?checksum64\s*=\s*)('.*')"     = "`${1}'$($Latest.Checksum64)'"
+      "(?i)(^\s*[$]?url64\s*=\s*)('.*')"          = "`${1}'$($Latest.URL64)'"
     }
   }
 }
@@ -23,13 +23,13 @@ function global:au_GetLatest {
   $Matches = $null
   $restAPI.tag_name -match '(\d+\.?)+'
   $version = $Matches[0]
-  $url32 = $restAPI.assets | Where-Object { ($_.content_type -eq 'application/x-msi') `
+  $url64 = $restAPI.assets | Where-Object { ($_.content_type -eq 'application/x-msi') `
     -and ($_.name -notlike '*nosse*' ) } `
     | Select-Object -First 1 -ExpandProperty browser_download_url
 
-  return @{ Version = $version; URL32 = $url32 }
+  return @{ Version = $version; URL64 = $url64 }
 }
 
 if ($MyInvocation.InvocationName -ne '.') { # run the update only if script is not sourced
-  Update-Package
+  Update-Package -ChecksumFor 64
 }
