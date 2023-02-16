b69281fa644073b5a8d07ca22860ba3b3a31656e
Chocolatey <chocolatey@realdimensions.net>
2023-02-16 00:07:40 +0000

    AU: 2 updated - hydrus-network plex [skip ci]

--- plex/tools/chocolateyinstall.ps1
+++ plex/tools/chocolateyinstall.ps1
@@ -4,10 +4,10 @@ $ErrorActionPreference = 'Stop'
 $packageArgs = @{
   packageName    = 'plex'
   fileType       = 'EXE'
-  url64          = 'https://downloads.plex.tv/plex-desktop/1.63.3.3523-1eff9563/windows/Plex-1.63.3.3523-1eff9563-x86_64.exe'
+  url64          = 'https://downloads.plex.tv/plex-desktop/1.64.2.3546-5107ec40/windows/Plex-1.64.2.3546-5107ec40-x86_64.exe'
   silentArgs     = '/S'
   softwareName   = 'Plex' 
-  checksum64     = '8a8ec045c200e14c6724f8398f98411d8ec6aad7'
+  checksum64     = '6dadd8559f96b8157d6caa19fc84e6bc8abbe29a'
   checksumType64 = 'sha1'
   validExitCodes = @(0, 1223)
 }
