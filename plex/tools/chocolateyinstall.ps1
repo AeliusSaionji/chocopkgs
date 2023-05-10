7c89ebea301552e35911af8588538262648cc2fd
Chocolatey <chocolatey@realdimensions.net>
2023-05-10 00:07:24 +0000

    AU: 3 updated - obs-studio obs-studio.install plex [skip ci]

--- plex/tools/chocolateyinstall.ps1
+++ plex/tools/chocolateyinstall.ps1
@@ -4,10 +4,10 @@ $ErrorActionPreference = 'Stop'
 $packageArgs = @{
   packageName    = 'plex'
   fileType       = 'EXE'
-  url64          = 'https://downloads.plex.tv/plex-desktop/1.67.2.3705-db506a00/windows/Plex-1.67.2.3705-db506a00-x86_64.exe'
+  url64          = 'https://downloads.plex.tv/plex-desktop/1.68.2.3746-7601b337/windows/Plex-1.68.2.3746-7601b337-x86_64.exe'
   silentArgs     = '/S'
   softwareName   = 'Plex' 
-  checksum64     = 'd20d6b0ad088ff790b884c8d220c8cda8ebce081'
+  checksum64     = 'd950bad36392e7f6879d305732179c1e5d52bc25'
   checksumType64 = 'sha1'
   validExitCodes = @(0, 1223)
 }
