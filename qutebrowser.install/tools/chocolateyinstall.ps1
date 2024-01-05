145bc01b05ed48e6b50d9b1d4caa1ad8604efb4f
Akira Yamamoto <3007213+akirayamamoto@users.noreply.github.com>
2024-01-06 04:31:31 +1100

    Fix qutebrowser. (#59)
    Looks good, thanks!

--- qutebrowser.install/tools/chocolateyinstall.ps1
+++ qutebrowser.install/tools/chocolateyinstall.ps1
@@ -4,11 +4,9 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
 	packageName = 'qutebrowser.install'
 	fileType    = 'exe'
-	file32      = "$toolsdir\qutebrowser-2.5.3-win32_x32.exe"
-	file64      = "$toolsdir\qutebrowser-2.5.3-amd64_x64.exe"
+	file64      = "$toolsdir\qutebrowser-3.1.0-amd64_x64.exe"
 	silentArgs  = '/S /allusers'
 }
 
 Install-ChocolateyInstallPackage @packageArgs
-Remove-Item $packageArgs.file32 -Force -ea 0
 Remove-Item $packageArgs.file64 -Force -ea 0
