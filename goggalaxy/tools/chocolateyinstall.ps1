8c0d63968937628ce4b823ac21eda5f5bd0abdd3
aelius <aelius@home>
2024-01-12 15:29:57 -0500

    goggalaxy manual update
    appveyor failed to run this for some reason?

--- goggalaxy/tools/chocolateyinstall.ps1
+++ goggalaxy/tools/chocolateyinstall.ps1
@@ -3,9 +3,9 @@
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   fileType      = 'EXE'
-  url           = 'https://content-system.gog.com/open_link/download?path=/open/galaxy/client/2.0.71.2/setup_galaxy_2.0.71.2.exe'
+  url           = 'https://content-system.gog.com/open_link/download?path=/open/galaxy/client/2.0.73.27/setup_galaxy_2.0.73.27.exe'
   softwareName  = 'GOG Galaxy*'
-  checksum      = 'c3708fa1513b00acf5dc928f387ff589'
+  checksum      = '1d6d6b76a8ed0d41a06771097a862e42'
   checksumType  = 'md5'
   silentArgs    = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
   validExitCodes= @(0,1) # Code 1 - Same or newer version already installed. Uninstall before trying to downgrade.
