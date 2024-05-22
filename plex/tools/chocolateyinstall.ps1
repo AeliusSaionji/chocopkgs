16ccff0851ab2d6ff811b1b91f3776e608eda544
Chocolatey <chocolatey@realdimensions.net>
2024-05-22 00:12:55 +0000

    AU: 3 updated - geforce-game-ready-driver nvidia-display-driver plex [skip ci]

--- plex/tools/chocolateyinstall.ps1
+++ plex/tools/chocolateyinstall.ps1
@@ -4,10 +4,10 @@ $ErrorActionPreference = 'Stop'
 $packageArgs = @{
   packageName    = 'plex'
   fileType       = 'EXE'
-  url64          = 'https://downloads.plex.tv/plex-desktop/1.92.1.140-8e1f68f5/windows/Plex-1.92.1.140-8e1f68f5-x86_64.exe'
+  url64          = 'https://downloads.plex.tv/plex-desktop/1.93.0.144-9b2f4a13/windows/Plex-1.93.0.144-9b2f4a13-x86_64.exe'
   silentArgs     = '/S'
   softwareName   = 'Plex' 
-  checksum64     = '4cea18465fa4e74e30a3b8f3029026a1dce98e2b'
+  checksum64     = 'f7e39d56f598395b11ef16c70a1966cc4a6727b2'
   checksumType64 = 'sha1'
   validExitCodes = @(0, 1223)
 }
