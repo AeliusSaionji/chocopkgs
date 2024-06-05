a343c264c3bb3922c0ca8c517c5c57a92e954a0a
Chocolatey <chocolatey@realdimensions.net>
2024-06-05 12:07:40 +0000

    AU: 1 updated - plex [skip ci]

--- plex/tools/chocolateyinstall.ps1
+++ plex/tools/chocolateyinstall.ps1
@@ -4,10 +4,10 @@ $ErrorActionPreference = 'Stop'
 $packageArgs = @{
   packageName    = 'plex'
   fileType       = 'EXE'
-  url64          = 'https://downloads.plex.tv/plex-desktop/1.93.0.144-9b2f4a13/windows/Plex-1.93.0.144-9b2f4a13-x86_64.exe'
+  url64          = 'https://downloads.plex.tv/plex-desktop/1.94.1.155-f1e7f457/windows/Plex-1.94.1.155-f1e7f457-x86_64.exe'
   silentArgs     = '/S'
   softwareName   = 'Plex' 
-  checksum64     = 'f7e39d56f598395b11ef16c70a1966cc4a6727b2'
+  checksum64     = 'f578b657b9e9515e9af05ea8bf02361bb50206c5'
   checksumType64 = 'sha1'
   validExitCodes = @(0, 1223)
 }
