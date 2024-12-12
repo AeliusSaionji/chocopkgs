4019a5db56b0b789d5f6c11aab31a6fd003bac50
David Gardiner <david@gardiner.net.au>
2024-12-12 13:15:34 +1030

    Check for Slack/Teams in OBS Studio installer

--- obs-studio.install/tools/chocolateyinstall.ps1
+++ obs-studio.install/tools/chocolateyinstall.ps1
@@ -2,6 +2,15 @@
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $file64Location = Get-Item $toolsDir\*_x64.exe
 
+# Check for running applications that may interfere with the installation
+if (Get-Process -Name ms-teams -ErrorAction SilentlyContinue) {
+  Write-Error "Please close Microsoft Teams before installing/updating OBS Studio."
+}
+
+if (Get-Process -Name Slack -ErrorAction SilentlyContinue) {
+  Write-Error "Please close Slack before installing/updating OBS Studio."
+}
+
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   fileType      = 'EXE'
