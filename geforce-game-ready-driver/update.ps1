de372beb4378421fc9fb7daede6b4057fd3b8027
Eugenio A. Naselli <enaselli@nvidia.com>
2026-01-29 23:57:21 +0100

    Correct urls for NVIDIA Geforce GameReady driver

--- geforce-game-ready-driver/update.ps1
+++ geforce-game-ready-driver/update.ps1
@@ -1,7 +1,7 @@
 ﻿Import-Module au
 
-$releases64 = 'https://gfwsl.geforce.com/services_toolkit/services/com/nvidia/services/AjaxDriverService.php?func=DriverManualLookup&psid=98&osID=57&languageCode=1033&beta=0&isWHQL=1&dltype=-1&dch=1&upCRD=0&sort1=0&numberOfResults=1'
-$releases7864 = 'https://gfwsl.geforce.com/services_toolkit/services/com/nvidia/services/AjaxDriverService.php?func=DriverManualLookup&psid=98&osID=19&languageCode=1033&beta=0&isWHQL=1&dltype=-1&dch=0&upCRD=0&sort1=0&numberOfResults=1'
+$releases64 = 'https://gfwsl.geforce.com/services_toolkit/services/com/nvidia/services/AjaxDriverService.php?func=DriverManualLookup&psid=98&pfid=942&osID=57&languageCode=1033&beta=0&isWHQL=0&dltype=-1&dch=1&upCRD=0&qnf=0&sort1=0&numberOfResults=1'
+$releases7864 = 'https://gfwsl.geforce.com/services_toolkit/services/com/nvidia/services/AjaxDriverService.php?func=DriverManualLookup&psid=120&pfid=985&osID=19&languageCode=1033&beta=0&isWHQL=0&dltype=-1&dch=0&upCRD=0&qnf=0&sort1=1&numberOfResults=1'
 
 function global:au_SearchReplace {
   @{
